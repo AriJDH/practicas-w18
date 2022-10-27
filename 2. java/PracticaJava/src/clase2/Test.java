0434955f8a60d696ca3f06caf238a6f434ed0514
jmartinperez <jmartin.perez@mercadolibre.com>
2022-10-27 12:59:37 -0300

    ejercicios de POO

--- 2. java/PracticaJava/src/clase2/Test.java
+++ 2. java/PracticaJava/src/clase2/Test.java
@@ -2,7 +2,7 @@ package clase2;
 
 public class Test {
     public static void main(String[] args) {
-        Jaguar jaguar = new Jaguar(10, 80);
+        Jaguar jaguar = new Jaguar(10,80);
         System.out.println(jaguar);
     }
 }
