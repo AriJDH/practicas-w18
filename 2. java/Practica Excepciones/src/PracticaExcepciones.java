d961647e37648a09b9cf93c9cb755c707ad74183
JonaaCaucota <jonathan.caucota@mercadolibre.com>
2022-10-27 15:37:26 -0300

    Ejercicio 1 terminado

---
+++ 2. java/Practica Excepciones/src/PracticaExcepciones.java
@@ -0,0 +1,18 @@
+public class PracticaExcepciones {
+    public static void main(String[] args) {
+        int a = 0;
+        int b = 300;
+
+        try {
+            int cociente = b / a;
+        }catch (ArithmeticException e){
+            throw new IllegalArgumentException("No se puede dividir por cero");
+        }finally {
+            System.out.println("Programa finalizado");
+        }
+
+    }
+
+
+
+}
