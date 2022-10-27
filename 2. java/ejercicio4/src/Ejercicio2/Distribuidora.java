12ec30cfbc3570b63dce4257f571b1e5e9fb898f
korec_micaela <micaelakorec@mercadolibre.com>
2022-10-27 19:05:23 -0300

    ejercicio integrador 4

---
+++ 2. java/ejercicio4/src/Ejercicio2/Distribuidora.java
@@ -0,0 +1,22 @@
+package Ejercicio2;
+
+import java.util.ArrayList;
+
+public class Distribuidora {
+    public static void main(String[] args) {
+
+        Perecedero perecedero = new Perecedero("Pepsi", 200, 2);
+        NoPerecedero noPerecedero = new NoPerecedero("Fanta", 175, "Bebida");
+
+        double sum = 0;
+        ArrayList<Producto> list = new ArrayList();
+        list.add(perecedero);
+        list.add(noPerecedero);
+
+        for (Producto item : list){
+            sum += item.calcular(5);
+        }
+        System.out.println("Total de los 10 productos: $" + sum);
+
+    }
+}
