38d5b498251c9946ac05eb0067dd48ee2cde8912
chpalomequer14 <christian.palomeque@mercadolibre.com.co>
2022-10-27 00:45:58 -0500

    Carrera de la selva

---
+++ 2. java/JavaP1Arrays/src/Participante.java
@@ -0,0 +1,15 @@
+public class Participante {
+
+    String Dni;
+    String Nombre;
+    String Apellido;
+    int Edad;
+    String Celular;
+    String NumeroDeEmergencias;
+    String GrupoSanguineo;
+    long Consecutivo;
+    float ValorPagar ;
+
+
+
+}
