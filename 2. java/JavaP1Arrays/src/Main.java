dca5f6fab9b3ec725e6122f70ef3a86de2703dda
chpalomequer14 <christian.palomeque@mercadolibre.com.co>
2022-10-27 12:31:53 -0500

    ejercicio 3

---
+++ 2. java/JavaP1Arrays/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+            Persona persona = new Persona();
+
+            Persona persona2 = new Persona("Martin", 20, "123456");
+
+            Persona persona3 = new Persona("Lucia", 22, "789456", 58, 160);
+
+            System.out.println(persona3.calcularIMC());
+
+            System.out.println(persona3);
+    }
+}
