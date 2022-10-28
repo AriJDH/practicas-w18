e5d8762cec8a7bac99a978f7138023a2485d29f0
mhmeli <milton.herrera@mercadolibre.com>
2022-10-28 16:34:02 -0300

    Java: poo-p1 Persona

---
+++ 2. java/poo-p1/src/poo/Main.java
@@ -0,0 +1,28 @@
+package poo;
+
+public class Main {
+    public static void main(String[] args) {
+        Persona persona1 = new Persona();
+        Persona persona2 = new Persona("Axel", 17, "3434343");
+        Persona persona3 = new Persona("Martin", 28, "43453454", 60, 1.70);
+
+        //Persona persona4 = new Persona("Felix", 34);
+        int imc = persona2.calcularIMC();
+        boolean mayor = persona2.esMayorDeEdad();
+
+        if(imc == -1) {
+            System.out.println("Bajo peso");
+        } else if(imc == 0){
+            System.out.println("Peso saludable");
+        } else {
+            System.out.println("Sobrepeso");
+        }
+
+        if(mayor) {
+            System.out.println("Es mayor");
+        } else {
+            System.out.println("Es menor");
+        }
+        System.out.println(persona3);
+    }
+}
