6856c5b0e2ce2504997bae3aca9e829f474dc08b
jmartinperez <jmartin.perez@mercadolibre.com>
2022-10-28 17:58:11 -0300

    ejercicio distribuidora

---
+++ 2. java/PracticaJava/Distribuidora/distribuidora/NoPerecederos.java
@@ -0,0 +1,35 @@
+package distribuidora;
+
+public class NoPerecederos extends Producto{
+    protected String tipo;
+    public NoPerecederos(String nombre, double precio,String tipo) {
+        super(nombre, precio);
+        this.tipo = tipo;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    @Override
+    public String toString() {
+        return "NoPerecederos{" +
+                "tipo='" + tipo + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", precio=" + precio +
+                '}';
+
+
+}
+
+    @Override
+    public double calcular(int cantidadDeProductos) {
+        return cantidadDeProductos*precio;
+    }
+
+
+}
