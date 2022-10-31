d8c94c5d56140f2be32a1fc8da86240e773c5952
Mariaduquee <mariapaula.duque@mercadolibre.com.co>
2022-10-31 07:43:33 -0500

    definicion arreglos

--- 2. java/TempGlobales/src/Paquete/Main.java
+++ 2. java/TempGlobales/src/Paquete/Main.java
@@ -1,8 +1,10 @@
 package Paquete;
 
 public class Main {
+    String ciudades[] = {"Londres", "Madrid", "Nueva york", "Buenos Aires", "Asuncion","Sao Paulo", "Lima", "Santiago de chile", "Lisboa", "Tokio"};
+    int temperaturas[][]= {{-2,33},{-3,32},{-8,27},{4,37},{6,42},{5,43},{0,39},{-7,26},{-1,31},{-10,35}};
 
     public static void main(String[] args) {
-	// write your code here
+
     }
 }
