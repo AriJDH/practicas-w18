8beb139a8f003cde65d6d536f8c9621b494d5138
mailenalsinameli <mailen.alsina@mercadolibre.com>
2022-11-01 16:47:30 -0300

    Ejercicio de temperatura terminado

--- 2. java/temperatura/src/Main.java
+++ 2. java/temperatura/src/Main.java
@@ -13,7 +13,28 @@ public class Main {
         int temperaturas[][] = {{-2,33},{-3,32},{-8,27},{4,37},{6,42},
                 {5,43},{0,39},{-7,26},{-1,31},{-10,35}};
 
+        int ciudadMenorTemperatura = 0;
+        int ciudadMayorTemperatura = 0;
 
+        for(int x = 0; x < temperaturas.length; x++){
+            for(int y = 0; y < temperaturas[x].length; y++){
+
+                if (temperaturas[x][y] < min){
+                    min = temperaturas[x][y];
+                    ciudadMenorTemperatura = x;
+
+                } if (temperaturas[x][y] > max) {
+                    max = temperaturas[x][y];
+                    ciudadMayorTemperatura = x;
+                }
+            }
+
+        }
+
+        System.out.println("La ciudad con menor temperatura es: " + ciudades[ciudadMenorTemperatura]);
+        System.out.println("La temperatura es: " + min);
+        System.out.println("La ciudad con mayor temperatura es: " + ciudades[ciudadMayorTemperatura]);
+        System.out.println("La temperatura es: " + max);
 
     }
 
