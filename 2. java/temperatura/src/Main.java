9a94fcee84cceb7eec18f317acebb4836aa87d5e
mailenalsinameli <mailen.alsina@mercadolibre.com>
2022-11-01 16:14:47 -0300

    matriz y arreglo de ciudades y temperaturas

--- 2. java/temperatura/src/Main.java
+++ 2. java/temperatura/src/Main.java
@@ -4,6 +4,17 @@ public class Main {
 
         System.out.println("Temperatura");
 
+        int max = 0;
+        int min = 0;
+
+        String ciudades[] = { "Londres", "Madrid", "Nueva York", "Buenos Aires",
+                "Asunción", "San Pablo", "Lima", "Santiago de Chile", "Lisboa", "Tokio"};
+
+        int temperaturas[][] = {{-2,33},{-3,32},{-8,27},{4,37},{6,42},
+                {5,43},{0,39},{-7,26},{-1,31},{-10,35}};
+
+
+
     }
 
 }
