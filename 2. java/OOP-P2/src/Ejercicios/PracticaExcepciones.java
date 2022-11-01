03bed460d84a3a65e56f5c375857dcb6fef4421f
Lucasrobaina01 <lucas.robaina@mercadolibre.com>
2022-11-01 15:59:19 -0300

    Practica Excepciones

---
+++ 2. java/OOP-P2/src/Ejercicios/PracticaExcepciones.java
@@ -0,0 +1,23 @@
+package Ejercicios;
+
+public class PracticaExcepciones {
+
+    private int a = 0;
+    private int b = 300;
+
+
+    public void calcular(){
+
+        try{
+            int result = this.b / this.a;
+        }
+        catch (ArithmeticException e){
+                throw new IllegalArgumentException("No se puede dividir por 0");
+        }
+    }
+
+
+
+
+
+}
