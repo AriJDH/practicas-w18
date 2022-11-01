bac1932f0a2944cc81b9e5bf389f05a31e5f1853
icingolani <ivanna.cingolani@mercadolibre.com>
2022-11-01 17:27:21 -0300

    Distribuidora refactor

--- 2. java/M8 Java P2/POO/src/DistribuidoraProductos/Distribuidora.java
+++ 2. java/M8 Java P2/POO/src/DistribuidoraProductos/Distribuidora.java
@@ -1,27 +1,64 @@
 package DistribuidoraProductos;
 
+import java.util.ArrayList;
+
 public class Distribuidora {
     public static void main(String[] args) {
 
-        Producto productos[] = new Producto[5];
 
-        productos[0] = new Perecedero("Leche", 10d, 1);
-        productos[1] = new Perecedero("Pan", 10d, 2);
-        productos[2] = new NoPerecedero("Lenteja", 10d, "Lata");
-        productos[3] = new NoPerecedero("Harina", 10d, "Paquete");
-        productos[4] = new Perecedero("Crema", 10d, 3);
+        ArrayList<Producto> productos = new ArrayList<>();
 
-        for (int i = 0; i < productos.length; i++) {
-            if(productos[i] instanceof Perecedero ) {
-                System.out.println("Producto Perecedero > Días por caducar: " + ((Perecedero) productos[i]).getDiasPorCaducar());
+        Producto perecedero1 = new Perecedero("Leche", 10d, 10);
+        Producto perecedero2 = new Perecedero("Pan", 10d, 10);
+        Producto noPerecedero3 = new NoPerecedero("Lenteja", 10d, "Lata");
+        Producto noPerecedero4 = new NoPerecedero("Harina", 10d, "Paquete");
+        Producto perecedero5 = new Perecedero("Crema", 10d, 10);
+
+        productos.add(perecedero1);
+        productos.add(perecedero2);
+        productos.add(noPerecedero3);
+        productos.add(noPerecedero4);
+        productos.add(perecedero5);
+
+
+        double sumaPerecedero = 0;
+        double sumaNoPerecedero = 0;
+        double totalCompra = 0;
+
+        for (Producto p : productos) {
+
+            mostrarProductos(p, 5);
+            if (p instanceof Perecedero) {
+                System.out.println("Producto Perecedero > Días por caducar: " + ((Perecedero) p).getDiasPorCaducar());
+                sumaPerecedero += p.calcular(5);
+                mostrarSuma("Parcial", p.getClass(), sumaPerecedero);
             } else {
-                System.out.println("Producto No Perecedero");
-            }
-            System.out.println("1 unidad de " + productos[i].getNombre() + " cuesta $" + productos[i].calcular(1));
+                System.out.println("Producto no perecedero");
+                sumaNoPerecedero += p.calcular(5);
+                mostrarSuma("Parcial", p.getClass(), sumaNoPerecedero);
 
-            System.out.println("2 unidad de " + productos[i].getNombre() + " cuesta $" + productos[i].calcular(2));
-            System.out.println(" ---  ");
+            }
+
+            totalCompra = sumaPerecedero + sumaNoPerecedero;
         }
 
+
+        System.out.println("--- ");
+
+        mostrarSuma("Total", Perecedero.class, sumaPerecedero);
+        mostrarSuma("Total", NoPerecedero.class, sumaNoPerecedero);
+
+        System.out.println("------------------------------ TOTAL $" + totalCompra);
     }
+
+    public static void mostrarProductos(Producto p, int cantidad){
+        System.out.println("---");
+        System.out.println(p.getNombre() + " $"  + p.getPrecio() + " c/u" + " - Total $" + p.calcular(cantidad));
+    }
+
+    public static void mostrarSuma(String tipo, Class<? extends Producto> aClass, double suma){
+        System.out.println( tipo + " Categoría: " + aClass.getSimpleName() + " $" + suma);
+    }
+
+
 }
