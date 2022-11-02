50b3d84bcc209f3fdb83e3107c2b30b683bc5997
mailenalsinameli <mailen.alsina@mercadolibre.com>
2022-11-02 17:34:34 -0300

    terminado

--- 2. java/carrera_selva/src/Main.java
+++ 2. java/carrera_selva/src/Main.java
@@ -1,5 +1,5 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
@@ -15,88 +15,100 @@ public class Main {
         String celular;
         String celularContacto;
         String grupoSanguineo;
-        String seguirCargando;
-        String circuito;
+        int opcion;
+        int contador = 0;
+        String seguirAgregando = "";
+
+        Map<Integer,String[]> listaParticipanteChico = new HashMap<>();
+        Map<Integer,String[]>listaParticipanteMedio = new HashMap<>();
+        Map<Integer,String[]> listaParticipanteGrande = new HashMap<>();
+
 
         System.out.println("¡Bienvenido/a a la carrera de la selva!");
-        System.out.println("Elija una opción. Presione 1 para el circuito chico, 2 para el medio y 3 para el avanzado.");
+        System.out.println("Para inscribirse, por favor, ingrese los siguientes datos: ");
 
-        int opcion = 0;
-        switch (opcion){
+        //input de datos
 
-            case 1:     System.out.println("Usted ha seleccionado el circuito chico");
-                        System.out.println("Para inscribirse, por favor, ingrese los siguientes datos: ");
+        do{
 
-                        //input de datos
+            System.out.println("Ingrese su Nombre: ");
+            nombre = input.next();
+            System.out.println("Ingrese su Apellido: ");
+            apellido = input.next();
+            System.out.println("Ingrese su Edad: ");
+            edad = input.next();
+            System.out.println("Ingrese su Dni: ");
+            dni = input.next();
+            System.out.println("Ingrese su Celular: ");
+            celular = input.next();
+            System.out.println("Ingrese su Celular de Contacto: ");
+            celularContacto = input.next();
+            System.out.println("Ingrese su Grupo sanguíneo: ");
+            grupoSanguineo = input.next();
 
-                        System.out.println("Ingrese su Nombre: ");
-                        nombre = input.next();
-                        System.out.println("Ingrese su Apellido: ");
-                        apellido = input.next();
-                        System.out.println("Ingrese su Edad: ");
-                        edad = input.next();
-                        System.out.println("Ingrese su Dni: ");
-                        dni = input.next();
-                        System.out.println("Ingrese su Celular: ");
-                        celular = input.next();
-                        System.out.println("Ingrese su Celular de Contacto: ");
-                        celularContacto = input.next();
-                        System.out.println("Ingrese su Grupo sanguíneo: ");
-                        grupoSanguineo = input.next();
-                        System.out.println("Ingrese el tipo de circuito que desea (chico, medio, avanzado): ");
-                        circuito = input.next();
+            System.out.println("Elija una opción. Presione 1 para el circuito chico, 2 para el medio y 3 para el avanzado.");
+            opcion = Integer.parseInt(input.next());
 
-                        String participante[] = {nombre, apellido, edad, dni, celular, celularContacto, grupoSanguineo, circuito};
+            String participante[] = {nombre, apellido, edad, dni, celular, celularContacto, grupoSanguineo};
 
-                        if (Integer.parseInt(edad) < 18){
-                            System.out.println("Usted debe abonar 1300 pesos.");
-                        } else if (Integer.parseInt(edad) > 18){
-                            System.out.println("Usted debe abonar 1500 pesos.");
-                        }
+            contador = contador + 1;
 
-                        List<String[]> listaParticipanteChico = new ArrayList<>();
-                        listaParticipanteChico.add(participante);
+            switch (opcion) {
 
-            case 2:     System.out.println("Usted ha seleccionado el circuito medio");
-                        System.out.println("Para inscribirse, por favor, ingrese los siguientes datos: ");
+                case 1:
 
-                        //input de datos
+                    if (Integer.parseInt(edad) < 18) {
+                        System.out.println("Usted debe abonar 1300 pesos.");
+                    } else if (Integer.parseInt(edad) > 18) {
+                        System.out.println("Usted debe abonar 1500 pesos.");
+                    }
 
-                        System.out.println("Ingrese su Nombre: ");
-                        nombre = input.next();
-                        System.out.println("Ingrese su Apellido: ");
-                        apellido = input.next();
-                        System.out.println("Ingrese su Edad: ");
-                        edad = input.next();
-                        System.out.println("Ingrese su Dni: ");
-                        dni = input.next();
-                        System.out.println("Ingrese su Celular: ");
-                        celular = input.next();
-                        System.out.println("Ingrese su Celular de Contacto: ");
-                        celularContacto = input.next();
-                        System.out.println("Ingrese su Grupo sanguíneo: ");
-                        grupoSanguineo = input.next();
-                        System.out.println("Ingrese el tipo de circuito que desea (chico, medio, avanzado): ");
-                        circuito = input.next();
+                    break;
 
-                        String participante2[] = {nombre, apellido, edad, dni, celular, celularContacto, grupoSanguineo, circuito};
+                case 2:
 
-                        if (Integer.parseInt(edad) < 18){
-                            System.out.println("Usted debe abonar 2000 pesos.");
-                        } else if (Integer.parseInt(edad) > 18){
-                            System.out.println("Usted debe abonar 2300 pesos.");
-                        }
+                    if (Integer.parseInt(edad) < 18) {
+                        System.out.println("Usted debe abonar 2000 pesos.");
+                    } else if (Integer.parseInt(edad) > 18) {
+                        System.out.println("Usted debe abonar 2300 pesos.");
+                    }
 
-                        List<String[]> listaParticipanteMedio = new ArrayList<>();
-                        listaParticipanteMedio.add(participante2);
+                    break;
 
+                case 3:
 
+                    if (Integer.parseInt(edad) < 18) {
+                        System.out.println("No se permite la inscripción.");
+                    } else if (Integer.parseInt(edad) > 18) {
+                        System.out.println("Usted debe abonar 2800 pesos.");
+                    }
+
+                    break;
+
+                default: System.out.println("El número ingresado no es el correcto");
+
+                    break;
+
+            }
+
+            listaParticipanteChico.put(contador, participante);
+
+            System.out.println("Desea agregar otro participante? (si/no) ");
+            seguirAgregando = input.next();
+
+        } while (seguirAgregando.equalsIgnoreCase("si"));
+
+        for(Map.Entry<Integer, String[] > entrada : listaParticipanteChico.entrySet()) {
+            Integer clave = entrada.getKey();
+            String[] valor = entrada.getValue();
+            System.out.println("La id del participante es " + clave);
+            System.out.println("La información del participante es: ");
+
+            for(int i = 0; i < valor.length; i++){
+                System.out.println(valor[i]);
+            }
         }
 
-
-            //for (String persona : participante1) {
-           //     System.out.println("Participante: " + persona);
-          //  }
 
     }
 
