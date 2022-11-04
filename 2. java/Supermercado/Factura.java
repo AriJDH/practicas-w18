1470912b2757adfa5fbcc3bf75b286df703a9def
Lucasrobaina01 <lucas.robaina@mercadolibre.com>
2022-11-04 13:14:03 -0300

    Update Factura.java

--- 2. java/Supermercado/Factura.java
+++ 2. java/Supermercado/Factura.java
@@ -3,7 +3,7 @@ package Supermercado;
 import Supermercado.Cliente;
 
 import java.util.List;
-/*Por otro lado, las facturas que se generan cuando un cliente
+/*Por otro lado, las facturas que se generan cuando n cliente
         hace una compra contienen a un cliente, una lista de ítems y el total de la compra.*/
 
 public class Factura {
