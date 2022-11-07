e4c0d8cfa4333b2a2f05607fbdcb6d2bc1c0f6d7
mailenalsinameli <mailen.alsina@mercadolibre.com>
2022-11-07 16:54:48 -0300

    constructor

--- 2. java/supermercado/src/Item.java
+++ 2. java/supermercado/src/Item.java
@@ -3,5 +3,8 @@ public class Item {
     private int codigo;
     private String nombre;
 
-
+    public Item(int codigo, String nombre) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+    }
 }
