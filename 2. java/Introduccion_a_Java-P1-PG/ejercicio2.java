53ccbe0581cc9ce4b8a66190be6e796d06dba2df
gabrivargas <gabriela.vargas@mercadolibre.com.co>
2022-11-07 15:50:27 -0500

    ejercicios practicos

---
+++ 2. java/Introduccion_a_Java-P1-PG/ejercicio2.java
@@ -0,0 +1,12 @@
+public class ejercicio2 {
+    public static void main(String[] args) {
+        String nombre;
+        int edad;
+        double sueldo;
+        String apellido;
+        String direccion;
+        boolean licencia_de_conducir;
+        double estatura_de_la_persona;
+        int cantidad_de_hijos;
+    }
+}
---
+++ 2. java/Introduccion_a_Java-P1-PG/ejericio1.java
@@ -0,0 +1,9 @@
+public class ejericio1 {
+    public static void main(String[] args) {
+        String apellido = "Gomez";
+        int edad = 35;
+        boolean example = false;
+        double sueldo = 45857.90;
+        String nombre = "Julián";
+    }
+}
