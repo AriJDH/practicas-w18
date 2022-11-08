7cd5e579a51da4d08a09d094776853b5c3ad6580
vamususue <vanessa.vmususue@mercadolibre.com.co>
2022-11-07 21:07:18 -0500

    Carpeta poo-p2 eliminada

--- 2. java/poo-p2/src/Ejercicio1/Main.java
+++
@@ -1,9 +0,0 @@
-package Ejercicio1;
-
-public class Main {
-    public static void main(String[] args) {
-
-        PracticaExcepciones x = new PracticaExcepciones();
-        x.calcular();
-    }
-}
