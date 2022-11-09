15880e471b74886ff3e98984ebb4da276afa4f5b
Svyatoslav Andriychenko <svyatoslav.andriychenko@mercadolibre.com>
2022-11-09 12:27:54 -0300

    inicia proyecto base maven

---
+++ 3. spring/clasespring/src/main/java/com/clasespring/clasespring/ClasespringApplication.java
@@ -0,0 +1,13 @@
+package com.clasespring.clasespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClasespringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClasespringApplication.class, args);
+	}
+
+}
