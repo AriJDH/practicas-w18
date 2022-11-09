0317e1c49adbb08e94047f1413fa5a2423c880c8
mainfantino <marcos.infantino@mercadolibre.com>
2022-11-09 13:02:24 -0300

    practica en vivo

---
+++ 3. spring/practicaSpringClaseVivo/src/main/java/com/example/practicaspringclasevivo/PracticaSpringClaseVivoApplication.java
@@ -0,0 +1,13 @@
+package com.example.practicaspringclasevivo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaSpringClaseVivoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticaSpringClaseVivoApplication.class, args);
+    }
+
+}
---
+++ 3. spring/practicaSpringClaseVivo/src/test/java/com/example/practicaspringclasevivo/PracticaSpringClaseVivoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practicaspringclasevivo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaSpringClaseVivoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
