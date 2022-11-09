406e3ba6411d78b975f6f4b85e94f377aba35b0f
Juanda0 <juandavid.echeverri@mercadolibre.com.co>
2022-11-09 12:33:46 -0500

    Resolucion ejercicio PG2, convertir un entero a romano usando Spring y REST API

---
+++ 3. spring/Ejercicio PG2/RTI-API/src/main/java/com/example/RTIAPI/RtiApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.RTIAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RtiApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RtiApiApplication.class, args);
+	}
+
+}
---
+++ 3. spring/Ejercicio PG2/RTI-API/src/test/java/com/example/RTIAPI/RtiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RTIAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RtiApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
