af307516296af309bc9f927d692a0844f4a90220
mhmeli <milton.herrera@mercadolibre.com>
2022-11-09 17:49:40 -0300

    Spring: Intro Spring p2 - Morse

---
+++ 3. spring/intro-spring-p2/morse/src/main/java/com/spring2/morse/controllers/MorseController.java
@@ -0,0 +1,61 @@
+package com.spring2.morse.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.*;
+
+@RestController
+public class MorseController {
+
+    private Map<String, Character> diccionarioMorse = new HashMap<>();
+
+    public MorseController() {
+        diccionarioMorse.put(".-", 'A');
+        diccionarioMorse.put("-...", 'B');
+        diccionarioMorse.put("-.-.", 'C');
+        diccionarioMorse.put("-..", 'D');
+        diccionarioMorse.put(".", 'E');
+        diccionarioMorse.put("..-.", 'F');
+        diccionarioMorse.put("--.", 'G');
+        diccionarioMorse.put("....", 'H');
+        diccionarioMorse.put("..", 'I');
+        diccionarioMorse.put(".---", 'J');
+        diccionarioMorse.put("-.-", 'K');
+        diccionarioMorse.put(".-..", 'L');
+        diccionarioMorse.put("--", 'M');
+        diccionarioMorse.put("-.", 'N');
+        diccionarioMorse.put("---", 'O');
+        diccionarioMorse.put(".--.", 'P');
+        diccionarioMorse.put("--.-", 'Q');
+        diccionarioMorse.put(".-.", 'R');
+        diccionarioMorse.put("...", 'S');
+        diccionarioMorse.put("-", 'T');
+        diccionarioMorse.put("..-", 'U');
+        diccionarioMorse.put("...-", 'V');
+        diccionarioMorse.put(".--", 'W');
+        diccionarioMorse.put("-..-", 'X');
+        diccionarioMorse.put("-.--", 'Y');
+        diccionarioMorse.put("--..", 'Z');
+        diccionarioMorse.put(" ", ' ');
+    }
+
+    @GetMapping("/morse/{code}")
+    public String morse(@PathVariable String code) {
+        String res = "";
+        String[] palabras = code.split("   ");
+
+        for (String palabra : palabras) {
+
+            ArrayList<String> letras = new ArrayList<>(Arrays.asList(palabra.split(" ")));
+
+            String s = letras.stream()
+                    .map(letra -> diccionarioMorse.get(letra).toString())
+                    .reduce("", String::concat);
+            res = res + s + " ";
+        }
+        System.out.println(res);
+        return res.substring(0, res.length() - 1);
+    }
+}
