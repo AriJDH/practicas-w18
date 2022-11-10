5f86f51dc8b3e887758e748c3140a1d648f81809
Juan Pablo Agudelo Florez <juanpablo.agudelo@mercadolibre.com.co>
2022-11-10 17:45:42 -0500

    Add solution IntroSpringP2Vivo

---
+++ 2. java/IntroSpringP2Vivo/src/main/java/com/meli/introspringp2vivo/IntroSpringP2VivoApplication.java
@@ -0,0 +1,13 @@
+package com.meli.introspringp2vivo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroSpringP2VivoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntroSpringP2VivoApplication.class, args);
+    }
+
+}
---
+++ 2. java/IntroSpringP2Vivo/src/test/java/com/meli/introspringp2vivo/IntroSpringP2VivoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meli.introspringp2vivo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroSpringP2VivoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
