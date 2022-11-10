b9b21895b5abfb84e03b4de4ddc262315101e705
Svyatoslav Andriychenko <svyatoslav.andriychenko@mercadolibre.com>
2022-11-10 11:35:31 -0300

    inicializo proyecto clase 2

---
+++ 3. spring/clase-2-dto/src/main/java/com/example/clase2dto/Clase2DtoApplication.java
@@ -0,0 +1,13 @@
+package com.example.clase2dto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Clase2DtoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Clase2DtoApplication.class, args);
+    }
+
+}
---
+++ 3. spring/clase-2-dto/src/test/java/com/example/clase2dto/Clase2DtoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clase2dto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Clase2DtoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
