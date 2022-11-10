6b6508e94f12820265254da2f6d52b10eff90acf
mailenalsinameli <mailen.alsina@mercadolibre.com>
2022-11-10 09:42:43 -0300

    codigo morse - terminado en grupo

---
+++ 3. spring/introduccionSpringP2Vivo/CodigoMorse/src/main/java/com/example/codigomorse/controller/MorseController.java
@@ -0,0 +1,16 @@
+package com.example.codigomorse.controller;
+
+import com.example.codigomorse.model.TraductorMorse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MorseController {
+
+    @GetMapping("/traducirMorse/{codigo}")
+    public String traducirMorse(@PathVariable String codigo){
+        //String res = TraductorMorse.traducirMorseALetra("-- . .-. -.-. .- -.. ---   .-.. .. -... .-. .");
+        return TraductorMorse.traducirMorseALetra(codigo);
+    }
+}
---
+++ 3. spring/introduccionSpringP2Vivo/CodigoMorse/src/main/java/com/example/codigomorse/model/TraductorMorse.java
@@ -0,0 +1,62 @@
+package com.example.codigomorse.model;
+
+import java.util.HashMap;
+
+public abstract class TraductorMorse {
+    private static HashMap<String, String> equivalencias= new HashMap<String, String>();
+
+    static{
+        equivalencias.put(".-.-.-",".");
+        equivalencias.put(".-","A");
+        equivalencias.put("-...","B");
+        equivalencias.put("-.-.","C");
+        equivalencias.put("-..","D");
+        equivalencias.put(".","E");
+        equivalencias.put("..-.","F");
+        equivalencias.put("--.","G");
+        equivalencias.put("....","H");
+        equivalencias.put("..","I");
+        equivalencias.put(".---","J");
+        equivalencias.put("-.-","K");
+        equivalencias.put(".-..","L");
+        equivalencias.put("--","M");
+        equivalencias.put("-.","N");
+        equivalencias.put("---","O");
+        equivalencias.put(".--.","P");
+        equivalencias.put("--.-","Q");
+        equivalencias.put(".-.","R");
+        equivalencias.put("...","S");
+        equivalencias.put("-","T");
+        equivalencias.put("..-","U");
+        equivalencias.put("...-","V");
+        equivalencias.put(".--","W");
+        equivalencias.put("-..-","X");
+        equivalencias.put("-.--","Y");
+        equivalencias.put("--..","Z");
+        equivalencias.put(".----","1");
+        equivalencias.put("..---","2");
+        equivalencias.put("...--","3");
+        equivalencias.put("....-","4");
+        equivalencias.put(".....","5");
+        equivalencias.put("_....","6");
+        equivalencias.put("__...","7");
+        equivalencias.put("___..","8");
+        equivalencias.put("____.","9");
+        equivalencias.put("_____","0");
+    }
+
+    public static String traducirMorseALetra(String strMorse){
+        String[] palabrasMorse = strMorse.split("   ");
+
+        String res = "";
+        for (String palabra:palabrasMorse) {
+            String[] letrasMorse = palabra.split(" ");
+
+            for(String letra: letrasMorse){
+                res += equivalencias.containsKey(letra)? equivalencias.get(letra):"[¿?]";
+            }
+            res += " ";
+        }
+        return res;
+    }
+}
