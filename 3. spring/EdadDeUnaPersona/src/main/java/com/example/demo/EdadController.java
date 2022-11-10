ea0344e66550883aa36c8ab176b42664f8e16a7f
JoseManuelSandona <jose.sandona@mercadolibre.com>
2022-11-10 12:13:45 -0300

    Ejercicio edad

---
+++ 3. spring/EdadDeUnaPersona/src/main/java/com/example/demo/EdadController.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/edad")
+public class EdadController {
+	
+	@GetMapping("/{dia}/{mes}/{anio}")
+	public String get(@PathVariable int dia, @PathVariable int mes, @PathVariable int anio) {
+		
+		LocalDate fecha = LocalDate.of(anio, mes, dia); 
+		
+		Period periodo = Period.between(fecha, LocalDate.now());
+		
+		String edad =  String.valueOf(periodo.getYears());
+		
+		return edad;
+	}
+
+}
