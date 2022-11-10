2fdefd35744c5082d2f8dd826e08c1a021018c7c
Ignacio Lobaiza Ramos <ignacio.lobaiza@mercadolibre.com>
2022-11-10 13:09:07 -0300

    Add ejercicio 1 de DTO y Response Entity

---
+++ 3. spring/edad-una-persona/src/main/java/meli/bootcamp/edadunapersona/EdadUnaPersonaApplication.java
@@ -0,0 +1,13 @@
+package meli.bootcamp.edadunapersona;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdadUnaPersonaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EdadUnaPersonaApplication.class, args);
+    }
+
+}
---
+++ 3. spring/edad-una-persona/src/test/java/meli/bootcamp/edadunapersona/EdadUnaPersonaApplicationTests.java
@@ -0,0 +1,13 @@
+package meli.bootcamp.edadunapersona;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdadUnaPersonaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
