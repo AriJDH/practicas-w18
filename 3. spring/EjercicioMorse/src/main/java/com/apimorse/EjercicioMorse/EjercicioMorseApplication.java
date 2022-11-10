d44cc55f3b049174d22d0083b4c4708ebfb9a1f8
Mariaduquee <mariapaula.duque@mercadolibre.com.co>
2022-11-10 14:36:52 -0500

    Creacion carpeta ejecicio morse

---
+++ 3. spring/EjercicioMorse/src/main/java/com/apimorse/EjercicioMorse/EjercicioMorseApplication.java
@@ -0,0 +1,13 @@
+package com.apimorse.EjercicioMorse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioMorseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjercicioMorseApplication.class, args);
+	}
+
+}
