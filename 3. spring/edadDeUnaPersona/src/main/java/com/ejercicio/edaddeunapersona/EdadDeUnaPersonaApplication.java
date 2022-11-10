7f1c692086056fc38bbdabd9e7e2070e038eb2b1
ayrestrepo <ayde.restrepo@mercadolibre.com.co>
2022-11-10 11:04:58 -0500

    Edad de una persona-solución

---
+++ 3. spring/edadDeUnaPersona/src/main/java/com/ejercicio/edaddeunapersona/EdadDeUnaPersonaApplication.java
@@ -0,0 +1,14 @@
+package com.ejercicio.edaddeunapersona;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdadDeUnaPersonaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EdadDeUnaPersonaApplication.class, args);
+    }
+
+
+}
---
+++ 3. spring/edadDeUnaPersona/src/main/java/com/ejercicio/edaddeunapersona/controlador/EdadPersonaController.java
@@ -0,0 +1,23 @@
+package com.ejercicio.edaddeunapersona.controlador;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+@RestController
+public class EdadPersonaController {
+
+        @GetMapping(path = "/edadPersona/{dia}/{mes}/{anio}/")
+        public String edadPersona(@PathVariable int dia, @PathVariable int mes, @PathVariable int anio) {
+
+            LocalDate fechaActual = LocalDate.now();
+            LocalDate fechaNacimiento = LocalDate.of(anio, mes, dia);
+
+            Period edad = Period.between(fechaNacimiento, fechaActual);
+            return String.valueOf(edad.getYears());
+
+    }
+}
