ec6ab6f113bb759be42a63b08318638bd2cdc961
Mariaduquee <mariapaula.duque@mercadolibre.com.co>
2022-11-10 08:43:31 -0500

    Creacion proyecto romanos

---
+++ 3. spring/numerosromanos/src/test/java/com/apiromanos/numerosromanos/NumerosromanosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apiromanos.numerosromanos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NumerosromanosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
