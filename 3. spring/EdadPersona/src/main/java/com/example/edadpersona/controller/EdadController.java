fa7cb64586114f30b8ec41b7617614311d3fd8ba
JonaaCaucota <jonathan.caucota@mercadolibre.com>
2022-11-10 13:26:58 -0300

    Ejercicio finalizado

---
+++ 3. spring/EdadPersona/src/main/java/com/example/edadpersona/controller/EdadController.java
@@ -0,0 +1,29 @@
+package com.example.edadpersona.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+
+@RestController
+public class EdadController {
+
+        @GetMapping("/{day}/{month}/{year}")
+        public String calcularEdad(@PathVariable String day, @PathVariable String month, @PathVariable String year){
+
+            String fdn = day + "/" + month + "/" + year;
+
+
+            DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+            LocalDate fechaNac = LocalDate.parse(fdn, fmt);
+            LocalDate hoy = LocalDate.now();
+
+            Period period = Period.between(fechaNac, hoy);
+
+            return String.valueOf(period.getYears());
+        }
+    }
+
