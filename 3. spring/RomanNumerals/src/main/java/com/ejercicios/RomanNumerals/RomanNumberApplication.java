3bf3c5062e5c14bf30a93d44a95353220bacf361
mhaller <melisa.haller@mercadolibre.com>
2022-11-10 23:28:49 -0300

    Numeros romanos de internet con modificaciones de grupo

---
+++ 3. spring/RomanNumerals/src/main/java/com/ejercicios/RomanNumerals/RomanNumberApplication.java
@@ -0,0 +1,13 @@
+package com.ejercicios.RomanNumerals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RomanNumberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RomanNumberApplication.class, args);
+	}
+
+}
