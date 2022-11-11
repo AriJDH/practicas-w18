b408ca88c0980e5448bce9b007bf4bfd61b19340
SebastianGines <sebastian.gines@mercadolibre.com>
2022-11-11 16:15:01 -0300

    Agregado de ejercicio multicapas realizado en vivo

---
+++ 3. spring/MultiCapaVivo/src/main/java/com/example/multicapavivo/service/IPersonaService.java
@@ -0,0 +1,9 @@
+package com.example.multicapavivo.service;
+
+import com.example.multicapavivo.dto.PersonaDto;
+
+import java.util.List;
+
+public interface IPersonaService {
+    List<PersonaDto> getPersonas();
+}
---
+++ 3. spring/MultiCapaVivo/src/main/java/com/example/multicapavivo/service/PersonaServiceImp.java
@@ -0,0 +1,27 @@
+package com.example.multicapavivo.service;
+
+import com.example.multicapavivo.dto.PersonaDto;
+import com.example.multicapavivo.entity.Persona;
+import com.example.multicapavivo.repository.IPersonaRepository;
+import com.example.multicapavivo.repository.PersonaRepositoryImp;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class PersonaServiceImp implements IPersonaService{
+
+    private IPersonaRepository personaRepository = new PersonaRepositoryImp();
+
+    @Override
+    public List<PersonaDto> getPersonas() {
+        ObjectMapper op = new ObjectMapper();
+        List<Persona> personas = personaRepository.findAll();
+        List<PersonaDto> personasDto = personas.stream()
+                .map(p -> op.convertValue(p, PersonaDto.class))
+                .collect(Collectors.toList());
+        return personasDto;
+    }
+}
