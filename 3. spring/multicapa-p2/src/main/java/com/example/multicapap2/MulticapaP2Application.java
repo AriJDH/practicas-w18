2d0c26529761479b1ddddda5b011dd65f1a9780b
Svyatoslav Andriychenko <svyatoslav.andriychenko@mercadolibre.com>
2022-11-11 15:52:41 -0300

    inicio de practicas de la tarde multicapa p2

---
+++ 3. spring/multicapa-p2/src/main/java/com/example/multicapap2/MulticapaP2Application.java
@@ -0,0 +1,13 @@
+package com.example.multicapap2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MulticapaP2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MulticapaP2Application.class, args);
+    }
+
+}
