be4e237579f650bb91aaf6dc1c35b2400c9bc8df
LeonardiGaston <gaston.leonardi@mercadolibre.com>
2022-11-12 10:42:05 -0300

    Add new Proyect StarsWars

---
+++ 3. spring/StarsWars/src/main/java/com/example/StarsWars/StarsWarsApplication.java
@@ -0,0 +1,13 @@
+package com.example.StarsWars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarsWarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarsWarsApplication.class, args);
+	}
+
+}
---
+++ 3. spring/StarsWars/src/test/java/com/example/StarsWars/StarsWarsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.StarsWars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarsWarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
