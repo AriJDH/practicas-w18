80df163c78c2dfdee29aa8ea9bb822bad2c781b7
LeonardiGaston <gaston.leonardi@mercadolibre.com>
2022-11-12 10:43:20 -0300

    Add new Entity Perosnaje in StarsWars

---
+++ 3. spring/StarsWars/src/main/java/com/example/StarsWars/entity/Personaje.java
@@ -0,0 +1,14 @@
+package com.example.StarsWars.entity;
+
+public class Personaje {
+    private String name;
+    private int height;
+    private int mass;
+    private String hairColor;
+    private String skinColor;
+    private String eyeColor;
+    private String brithYear;
+    private String gender;
+    private String homeworld;
+    private String species;
+}
