3b36300657d0d064ef32fc6b66d8e121c4679645
aagaard-meli <martin.aagaard@mercadolibre.com>
2022-11-15 18:16:26 -0300

    3-spring 22-multicapa-p2-vivo cardealership (solo readme)

---
+++ 3. spring/22-multicapa-p2-vivo/cardealership/src/main/java/com/vroom/cardealership/CardealershipApplication.java
@@ -0,0 +1,13 @@
+package com.vroom.cardealership;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardealershipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CardealershipApplication.class, args);
+    }
+
+}
---
+++ 3. spring/22-multicapa-p2-vivo/cardealership/src/test/java/com/vroom/cardealership/CardealershipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vroom.cardealership;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardealershipApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
