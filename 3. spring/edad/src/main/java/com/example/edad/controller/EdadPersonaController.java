13372703588058c80892f557f7cb1cd5223ad344
iMrJulianMELI <julian.rincon@mercadolibre.com.co>
2022-11-15 08:42:52 -0500

    Ejercicio de edad de una persona con DTO y Response Entity

--- 3. spring/edad/src/main/java/com/example/edad/controller/EdadPersonaController.java
+++ 3. spring/edad/src/main/java/com/example/edad/controller/EdadPersonaController.java
@@ -1,17 +1,24 @@
 package com.example.edad.controller;
 
-public class controller {
 
-    @RestController
-    public class EdadPersonaController {
-        @GetMapping(path = "/edadPersona/{dia}/{mes}/{anio}/")
-        public String edadPersona(@PathVariable int dia, @PathVariable int mes, @PathVariable int anio) {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
-            LocalDate fechaActual = LocalDate.now();
-            LocalDate fechaNacimiento = LocalDate.of(anio, mes, dia);
+import java.time.LocalDate;
+import java.time.Period;
 
-            Period edad = Period.between(fechaNacimiento, fechaActual);
-            return String.valueOf(edad.getYears());
-        }
+@RestController
+public class EdadPersonaController {
+    @GetMapping(path = "/edadPersona/{dia}/{mes}/{anio}")
+    public String edadPersona(@PathVariable int dia, @PathVariable int mes, @PathVariable int anio) {
+
+        System.out.println("Voy a calcular la edad");
+        LocalDate fechaActual = LocalDate.now();
+        LocalDate fechaNacimiento = LocalDate.of(anio, mes, dia);
+
+        Period edad = Period.between(fechaNacimiento, fechaActual);
+        return String.valueOf(edad.getYears());
     }
 }
+
