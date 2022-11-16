58f45cf6623a42e948c4758149d3153714640420
DizziTren2 <dizzi.tren@mercadolibre.com.co>
2022-11-16 15:13:16 -0500

    update type interface method

--- 4. sprint/be_java_hisp_w18_g03/src/main/java/com/sprint1/be_java_hisp_w18_g03/Repository/IUserRepository.java
+++ 4. sprint/be_java_hisp_w18_g03/src/main/java/com/sprint1/be_java_hisp_w18_g03/Repository/IUserRepository.java
@@ -5,7 +5,7 @@ import com.sprint1.be_java_hisp_w18_g03.entity.User;
 import java.util.List;
 
 public interface IUserRepository {
-    User findById(int userid);
+    User findById(Integer userid);
 
     List<User> selectAll();
 
