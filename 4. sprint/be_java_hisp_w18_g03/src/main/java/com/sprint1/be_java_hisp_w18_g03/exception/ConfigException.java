bab73a516dbc410b8326a85fb75a6e7214c6a37b
DizziTren2 <dizzi.tren@mercadolibre.com.co>
2022-11-16 14:00:47 -0500

    update return no found exception

--- 4. sprint/be_java_hisp_w18_g03/src/main/java/com/sprint1/be_java_hisp_w18_g03/exception/ConfigException.java
+++ 4. sprint/be_java_hisp_w18_g03/src/main/java/com/sprint1/be_java_hisp_w18_g03/exception/ConfigException.java
@@ -1,5 +1,6 @@
 package com.sprint1.be_java_hisp_w18_g03.exception;
 
+import com.sprint1.be_java_hisp_w18_g03.dto.response.ResponseDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -10,6 +11,6 @@ public class ConfigException {
 
     @ExceptionHandler
     public ResponseEntity<?> noFoundUserException(NoFoundUserException noFoundUserException) {
-        return null;
+        return new ResponseEntity<>(new ResponseDTO(noFoundUserException.getMessage(), HttpStatus.NOT_FOUND.value()), HttpStatus.NOT_FOUND);
     }
 }
