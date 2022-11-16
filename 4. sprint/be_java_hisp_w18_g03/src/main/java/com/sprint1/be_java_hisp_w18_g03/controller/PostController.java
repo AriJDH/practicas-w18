7a0490ae6eaf815df31f47f53765a0485bbb9926
iMrJulianMELI <julian.rincon@mercadolibre.com.co>
2022-11-16 15:00:48 -0500

    add DI in controllers

--- 4. sprint/be_java_hisp_w18_g03/src/main/java/com/sprint1/be_java_hisp_w18_g03/controller/PostController.java
+++ 4. sprint/be_java_hisp_w18_g03/src/main/java/com/sprint1/be_java_hisp_w18_g03/controller/PostController.java
@@ -1,9 +1,13 @@
 package com.sprint1.be_java_hisp_w18_g03.controller;
 
+import com.sprint1.be_java_hisp_w18_g03.service.IPostService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/products")
 public class PostController {
+    @Autowired
+    private IPostService postService;
 }
--- 4. sprint/be_java_hisp_w18_g03/src/main/java/com/sprint1/be_java_hisp_w18_g03/controller/UserController.java
+++ 4. sprint/be_java_hisp_w18_g03/src/main/java/com/sprint1/be_java_hisp_w18_g03/controller/UserController.java
@@ -1,9 +1,15 @@
 package com.sprint1.be_java_hisp_w18_g03.controller;
 
+import com.sprint1.be_java_hisp_w18_g03.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
+    @Autowired
+    private IUserService userService;
+
+
 }
