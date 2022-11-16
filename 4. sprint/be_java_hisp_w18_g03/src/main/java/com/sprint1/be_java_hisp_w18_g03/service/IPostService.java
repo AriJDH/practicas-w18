16024ca0ef0ef4d778c929cc012e90dad757a035
juagudelo <juanpablo.agudelo@mercadolibre.com.co>
2022-11-16 14:43:25 -0500

    add imports in service

--- 4. sprint/be_java_hisp_w18_g03/src/main/java/com/sprint1/be_java_hisp_w18_g03/service/IPostService.java
+++ 4. sprint/be_java_hisp_w18_g03/src/main/java/com/sprint1/be_java_hisp_w18_g03/service/IPostService.java
@@ -1,9 +1,14 @@
 package com.sprint1.be_java_hisp_w18_g03.service;
 
+
+import com.sprint1.be_java_hisp_w18_g03.dto.request.RequestPostDTO;
+import com.sprint1.be_java_hisp_w18_g03.dto.response.ResponseDTO;
+import com.sprint1.be_java_hisp_w18_g03.dto.response.SellersPostDTO;
+
 public interface IPostService {
 
     ResponseDTO createPost(RequestPostDTO request);
-    SellerPostDTO getPostSellet(Integer userId);
 
+    SellersPostDTO getPostSellet(Integer userId);
 
 }
