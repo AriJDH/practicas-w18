471ea647a983632bde3f46a5e3622326abbaacd4
Daniel-Cagliari <daniel.cagliari@mercadolibre.com>
2022-11-16 17:25:15 -0300

    adding BodyLessException class and implemented on ExceptionConfig class

--- 4. sprint/be_java_hisp_w18_g10/src/main/java/com/dh/be_java_hisp_w18_g10/exception/ExceptionConfig.java
+++ 4. sprint/be_java_hisp_w18_g10/src/main/java/com/dh/be_java_hisp_w18_g10/exception/ExceptionConfig.java
@@ -1,7 +1,18 @@
 package com.dh.be_java_hisp_w18_g10.exception;
 
+import com.dh.be_java_hisp_w18_g10.dto.response.ErrorDTOres;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class ExceptionConfig {
+
+    @ExceptionHandler(BodyLessException.class)
+    public ResponseEntity<?> blogEntryNotFoundException(BodyLessException e){
+        ErrorDTOres err = new ErrorDTOres(e.getMessage(), HttpStatus.BAD_REQUEST.value());
+        return new ResponseEntity<>(err, HttpStatus.BAD_REQUEST);
+    }
+
 }
