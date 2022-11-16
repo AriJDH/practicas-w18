25b63ed3059174051c2134554b3317f5108568ab
iMrJulianMELI <julian.rincon@mercadolibre.com.co>
2022-11-16 14:57:30 -0500

    Request mapping in controllers

--- 4. sprint/be_java_hisp_w18_g03/src/main/java/com/sprint1/be_java_hisp_w18_g03/controller/PostController.java
+++ 4. sprint/be_java_hisp_w18_g03/src/main/java/com/sprint1/be_java_hisp_w18_g03/controller/PostController.java
@@ -1,7 +1,9 @@
 package com.sprint1.be_java_hisp_w18_g03.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/products")
 public class PostController {
 }
