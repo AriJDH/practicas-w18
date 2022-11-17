7e64268e4e1fa98003db574f404f878ed9174498
mhmeli <milton.herrera@mercadolibre.com>
2022-11-17 15:26:34 -0300

    Group US finished

---
+++ 4. sprint/be_java_hisp_w18_g02_herrera/src/main/java/com/mercadolibre/socialmeli/dto/ErrorDto.java
@@ -0,0 +1,13 @@
+package com.mercadolibre.socialmeli.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class ErrorDto {
+    private String message;
+    private int status;
+}
---
+++ 4. sprint/be_java_hisp_w18_g02_herrera/src/main/java/com/mercadolibre/socialmeli/entity/Product.java
@@ -0,0 +1,18 @@
+package com.mercadolibre.socialmeli.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Product {
+    private Integer id;
+    private String name;
+    private String type;
+    private String brand;
+    private String color;
+    private String notes;
+
+}
