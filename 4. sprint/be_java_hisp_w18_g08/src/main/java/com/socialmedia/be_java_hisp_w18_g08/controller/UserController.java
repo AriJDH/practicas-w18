9cc46a7fae10bbc84da7ccb5a8ca4333393e05a1
vamususue <vanessa.vmususue@mercadolibre.com.co>
2022-11-17 09:47:46 -0500

    Endpoint to follow request created

--- 4. sprint/be_java_hisp_w18_g08/src/main/java/com/socialmedia/be_java_hisp_w18_g08/controller/UserController.java
+++ 4. sprint/be_java_hisp_w18_g08/src/main/java/com/socialmedia/be_java_hisp_w18_g08/controller/UserController.java
@@ -1,7 +1,23 @@
 package com.socialmedia.be_java_hisp_w18_g08.controller;
 
+import com.socialmedia.be_java_hisp_w18_g08.dto.FollowDtoReq;
+import com.socialmedia.be_java_hisp_w18_g08.dto.FollowDtoRes;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
+
+    @PostMapping("/{userId}/follow/{userIdToFollow}")
+    public ResponseEntity<FollowDtoRes> follow (@PathVariable Integer userId, @PathVariable Integer userIdToFollow) {
+        FollowDtoReq followDtoReq = new FollowDtoReq(userId, userIdToFollow);
+        return new ResponseEntity(followDtoReq, HttpStatus.OK);
+
+    }
+
 }
