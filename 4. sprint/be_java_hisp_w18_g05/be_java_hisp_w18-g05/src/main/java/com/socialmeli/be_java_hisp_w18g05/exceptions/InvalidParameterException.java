2e18f9565e1a15f5f73ea1007d7aa774b045015b
Sebastián Gines <sebastian.gines@mercadolibre.com>
2022-11-17 17:51:04 -0300

    Agregado de proyecto global e inicio de proyecto personal

---
+++ 4. sprint/be_java_hisp_w18_g05/be_java_hisp_w18-g05/src/main/java/com/socialmeli/be_java_hisp_w18g05/exceptions/InvalidParameterException.java
@@ -0,0 +1,7 @@
+package com.socialmeli.be_java_hisp_w18g05.exceptions;
+
+public class InvalidParameterException extends RuntimeException{
+    public InvalidParameterException(String message) {
+        super(message);
+    }
+}
