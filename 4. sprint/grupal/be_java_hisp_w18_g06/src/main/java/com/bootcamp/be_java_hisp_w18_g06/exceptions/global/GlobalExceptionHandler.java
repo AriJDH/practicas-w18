1c0e0cc584aa307aaf6abca2dd5fc79ab35253e6
Angel Fernandez <angel.afernandez@mercadolibre.com>
2022-11-17 10:11:37 -0300

    added Exceptions

---
+++ 4. sprint/grupal/be_java_hisp_w18_g06/src/main/java/com/bootcamp/be_java_hisp_w18_g06/exceptions/global/GlobalExceptionHandler.java
@@ -0,0 +1,37 @@
+package com.bootcamp.be_java_hisp_w18_g06.exceptions.global;
+
+import com.bootcamp.be_java_hisp_w18_g06.dto.response.exception.ExceptionResponseDto;
+import com.bootcamp.be_java_hisp_w18_g06.exceptions.BadRequestException;
+import com.bootcamp.be_java_hisp_w18_g06.exceptions.EmptyException;
+import com.bootcamp.be_java_hisp_w18_g06.exceptions.NotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+
+@ControllerAdvice(annotations = RestController.class)
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<ExceptionResponseDto> badRequestException(BadRequestException e) {
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new ExceptionResponseDto(e.getMessage()
+                        , HttpStatus.BAD_REQUEST.value()));
+    }
+    @ExceptionHandler(EmptyException.class)
+    public ResponseEntity<ExceptionResponseDto> emptyException(EmptyException e) {
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new ExceptionResponseDto(e.getMessage()
+                        , HttpStatus.NO_CONTENT.value()));
+    }
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<ExceptionResponseDto> notFoundException(NotFoundException e) {
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new ExceptionResponseDto(e.getMessage()
+                        , HttpStatus.NOT_FOUND.value()));
+    }
+}
