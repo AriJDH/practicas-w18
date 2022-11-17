bfe6f638eff1f5b7f2f370cb77b2a8a9ad3a372e
mhmeli <milton.herrera@mercadolibre.com>
2022-11-17 10:21:20 -0300

    Changed follow and unfollowed methods: get to post

--- 4. sprint/be_java_hisp_w18_g02/src/main/java/com/mercadolibre/socialmeli/controller/UserController.java
+++ 4. sprint/be_java_hisp_w18_g02/src/main/java/com/mercadolibre/socialmeli/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,12 +19,12 @@ public class UserController {
     public ResponseEntity<SellerFollowerCountDtoRes> getCount(@PathVariable Integer userId) {
         return ResponseEntity.status(HttpStatus.OK).body(userService.getCount(userId));
     }
-    @GetMapping("/users/{userId}/follow/{userIdToFollow}")
+    @PostMapping("/users/{userId}/follow/{userIdToFollow}")
     public ResponseEntity<?> follow(@PathVariable Integer userId, @PathVariable Integer userIdToFollow){
         return new ResponseEntity<>(userService.follow(userId, userIdToFollow), HttpStatus.valueOf(200));
     }
 
-    @GetMapping("/users/{userId}/unfollow/{userIdToFollow}")
+    @PostMapping("/users/{userId}/unfollow/{userIdToFollow}")
     public ResponseEntity<?> unfollow(@PathVariable Integer userId, @PathVariable Integer userIdToFollow){
         return new ResponseEntity<>(userService.unfollow(userId, userIdToFollow), HttpStatus.valueOf(200));
     }
