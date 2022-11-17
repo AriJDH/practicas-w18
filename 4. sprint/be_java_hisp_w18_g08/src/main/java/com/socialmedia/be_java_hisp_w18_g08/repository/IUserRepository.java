8e025f8105b4c4020ee3140823f42d4995a2f5e0
jootero <jose.otero@mercadolibre.com>
2022-11-17 11:02:23 -0300

    Exception handler creation, and implementation of the previusly created methods

--- 4. sprint/be_java_hisp_w18_g08/src/main/java/com/socialmedia/be_java_hisp_w18_g08/repository/IUserRepository.java
+++ 4. sprint/be_java_hisp_w18_g08/src/main/java/com/socialmedia/be_java_hisp_w18_g08/repository/IUserRepository.java
@@ -1,4 +1,10 @@
 package com.socialmedia.be_java_hisp_w18_g08.repository;
 
+import com.socialmedia.be_java_hisp_w18_g08.entity.User;
+
+import java.util.Optional;
+
 public interface IUserRepository {
+
+    User getUserByID(Integer userId);
 }
--- 4. sprint/be_java_hisp_w18_g08/src/main/java/com/socialmedia/be_java_hisp_w18_g08/service/UserServiceImp.java
+++ 4. sprint/be_java_hisp_w18_g08/src/main/java/com/socialmedia/be_java_hisp_w18_g08/service/UserServiceImp.java
@@ -1,9 +1,16 @@
 package com.socialmedia.be_java_hisp_w18_g08.service;
 
 import com.socialmedia.be_java_hisp_w18_g08.dto.FollowedDTO;
+import com.socialmedia.be_java_hisp_w18_g08.dto.SellerDTO;
+import com.socialmedia.be_java_hisp_w18_g08.entity.Seller;
+import com.socialmedia.be_java_hisp_w18_g08.entity.User;
 import com.socialmedia.be_java_hisp_w18_g08.repository.IUserRepository;
 import com.socialmedia.be_java_hisp_w18_g08.repository.UserRepositoryImp;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserServiceImp implements IUserService{
@@ -16,6 +23,20 @@ public class UserServiceImp implements IUserService{
 
     @Override
     public FollowedDTO getFollowed(Integer userId) {
-        return null;
+        User user =  userRepository.getUserByID(userId);
+        FollowedDTO response = new FollowedDTO();
+        response.setUser_id(user.getUser_id());
+        response.setUser_name(user.getUser_name());
+        response.setFollowed(createSellerDTOList(user.getFollowed()));
+        return response;
+    }
+
+    private List<SellerDTO> createSellerDTOList(List<Seller> followed) {
+        List<SellerDTO> sellers = new ArrayList<>();
+        for(Seller s : followed){
+            SellerDTO aux = new SellerDTO(s.getUser_id(), s.getUser_name());
+            sellers.add(aux);
+        }
+        return sellers;
     }
 }
