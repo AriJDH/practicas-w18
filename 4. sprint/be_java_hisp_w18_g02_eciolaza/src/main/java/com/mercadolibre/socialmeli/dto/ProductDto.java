2d8cecda53897c1b0337a5c2716459bb88937b9a
agustinaeciolaza <agustina.eciolaza@mercadolibre.com>
2022-11-17 18:30:25 -0300

    nuevo todo

---
+++ 4. sprint/be_java_hisp_w18_g02_eciolaza/src/main/java/com/mercadolibre/socialmeli/dto/ProductDto.java
@@ -0,0 +1,20 @@
+package com.mercadolibre.socialmeli.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class ProductDto {
+    @JsonProperty("product_id")
+    private Integer id;
+    @JsonProperty("product_name")
+    private String name;
+    private String type;
+    private String brand;
+    private String color;
+    private String notes;
+}
