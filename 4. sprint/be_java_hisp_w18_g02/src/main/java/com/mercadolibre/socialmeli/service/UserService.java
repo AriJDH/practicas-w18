f19df0925e94c5c96f60b084a3f6e189b0a2495d
Gabriel Guerra <gabriel.guerra@mercadolibre.com>
2022-11-17 11:57:36 -0300

    User Story 5 tested

--- 4. sprint/be_java_hisp_w18_g02/src/main/java/com/mercadolibre/socialmeli/service/UserService.java
+++ 4. sprint/be_java_hisp_w18_g02/src/main/java/com/mercadolibre/socialmeli/service/UserService.java
@@ -1,5 +1,15 @@
 package com.mercadolibre.socialmeli.service;
 
+import com.mercadolibre.socialmeli.dto.request.PostDtoReq;
+import com.mercadolibre.socialmeli.dto.response.SellerFollowerCountDtoRes;
+import com.mercadolibre.socialmeli.dto.response.SellerFollowerListDtoRes;
+import com.mercadolibre.socialmeli.dto.response.UserDtoRes;
+import com.mercadolibre.socialmeli.dto.response.UserFollowedListDtoRes;
+import com.mercadolibre.socialmeli.entity.Post;
+import com.mercadolibre.socialmeli.entity.Product;
+import com.mercadolibre.socialmeli.entity.User;
+import com.mercadolibre.socialmeli.exception.BadRequestException;
+import com.mercadolibre.socialmeli.exception.NotFoundException;
 import com.mercadolibre.socialmeli.repository.IUserRepository;
 import com.mercadolibre.socialmeli.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,22 +27,29 @@ public class UserService implements IUserService{
 
     @Override
     public void addPost(PostDtoReq postReq) {
+        Post post;
+        Product prod;
         try {
-            Product prod = new Product(postReq.getProduct().getId(),
+            prod = new Product(postReq.getProduct().getId(),
                     postReq.getProduct().getName(),
                     postReq.getProduct().getType(),
                     postReq.getProduct().getBrand(),
                     postReq.getProduct().getColor(),
                     postReq.getProduct().getNotes());
-            Post post = new Post(userRepository.getNextPostId(),
+            post = new Post(userRepository.getNextPostId(),
                     postReq.getDate(),
                     postReq.getCategory(),
                     postReq.getPrice(),
                     prod);
             userRepository.createPost(postReq.getUserId(), post);
-        } catch (Exception e){
+
+        } catch (NotFoundException nf){
+            throw nf;
+        }
+        catch (Exception e){
             throw new BadRequestException("Posteo invalido");
         }
+
     }
 
     @Override
