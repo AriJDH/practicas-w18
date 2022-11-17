39bbd700fdecba9fa70910e9ca38d9d5893fd9ee
mjperez89 <jonathanmartinperez1989@gmail.com>
2022-11-17 17:43:52 -0300

    upload sprint 1

---
+++ 4. sprint/be_java_hisp_w18_g05/be_java_hisp_w18-g05/src/main/java/com/socialmeli/be_java_hisp_w18g05/entity/Buyer.java
@@ -0,0 +1,36 @@
+package com.socialmeli.be_java_hisp_w18g05.entity;
+
+
+import com.socialmeli.be_java_hisp_w18g05.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Data
+public class Buyer extends User {
+
+    private List<Seller> followeds;
+
+    public Buyer(Integer user_id, String name) {
+        super(user_id, name);
+        this.followeds = new ArrayList<>();
+    }
+
+    public Buyer(Integer user_id, String name, List<Seller> followeds) {
+        super(user_id, name);
+        this.followeds = followeds;
+    }
+
+    public void addFollowed(Seller seller){
+        followeds.add(seller);
+    }
+
+    public void unFollowed(Seller seller){
+        followeds.remove(seller);
+    }
+}
+
