e7d4aa535d64da162fed40a0e7bea2139b44579a
Angel Fernandez <angel.afernandez@mercadolibre.com>
2022-11-17 11:07:40 -0300

    add JsonFormat to LocalDate

--- 4. sprint/grupal/be_java_hisp_w18_g06/src/main/java/com/bootcamp/be_java_hisp_w18_g06/entity/Post.java
+++ 4. sprint/grupal/be_java_hisp_w18_g06/src/main/java/com/bootcamp/be_java_hisp_w18_g06/entity/Post.java
@@ -1,5 +1,6 @@
 package com.bootcamp.be_java_hisp_w18_g06.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,6 +13,7 @@ import java.time.LocalDate;
 public class Post {
     private int post_id;
     private int user_id;
+    @JsonFormat(pattern = "dd-MM-yyyy")
     private LocalDate date;
     private Product product;
     private String category;
