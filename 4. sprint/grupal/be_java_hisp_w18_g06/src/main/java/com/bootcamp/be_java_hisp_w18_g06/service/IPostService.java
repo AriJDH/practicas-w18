6038fa718c9a5d38d6a8da7fef1fb06ad29e2aaa
Angel Fernandez <angel.afernandez@mercadolibre.com>
2022-11-17 12:42:38 -0300

    added Mapper class

--- 4. sprint/grupal/be_java_hisp_w18_g06/src/main/java/com/bootcamp/be_java_hisp_w18_g06/service/IPostService.java
+++ 4. sprint/grupal/be_java_hisp_w18_g06/src/main/java/com/bootcamp/be_java_hisp_w18_g06/service/IPostService.java
@@ -1,4 +1,8 @@
 package com.bootcamp.be_java_hisp_w18_g06.service;
 
+import com.bootcamp.be_java_hisp_w18_g06.dto.request.PostDTO;
+import com.bootcamp.be_java_hisp_w18_g06.entity.Post;
+
 public interface IPostService {
+
 }
--- 4. sprint/grupal/be_java_hisp_w18_g06/src/main/java/com/bootcamp/be_java_hisp_w18_g06/service/imp/PostService.java
+++ 4. sprint/grupal/be_java_hisp_w18_g06/src/main/java/com/bootcamp/be_java_hisp_w18_g06/service/imp/PostService.java
@@ -1,5 +1,6 @@
 package com.bootcamp.be_java_hisp_w18_g06.service.imp;
 
+import com.bootcamp.be_java_hisp_w18_g06.dto.request.PostDTO;
 import com.bootcamp.be_java_hisp_w18_g06.repository.IUserRepository;
 import com.bootcamp.be_java_hisp_w18_g06.service.IPostService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,4 +11,5 @@ public class PostService implements IPostService {
 
     @Autowired
     private IUserRepository userRepository;
+
 }
