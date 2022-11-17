fd94bb1db7ec99617ea4544381dfaaf79a667df1
jootero <jose.otero@mercadolibre.com>
2022-11-17 09:18:13 -0300

    Creation of the endpoint, service and the DTOs that will be used to respond

---
+++ 4. sprint/be_java_hisp_w18_g08/src/main/java/com/socialmedia/be_java_hisp_w18_g08/dto/FollowedDTO.java
@@ -0,0 +1,20 @@
+package com.socialmedia.be_java_hisp_w18_g08.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class FollowedDTO {
+
+    private Integer user_id;
+    private String user_name;
+    private List<SellerDTO> followed;
+
+}
--- 4. sprint/be_java_hisp_w18_g08/src/main/java/com/socialmedia/be_java_hisp_w18_g08/service/UserServiceImp.java
+++ 4. sprint/be_java_hisp_w18_g08/src/main/java/com/socialmedia/be_java_hisp_w18_g08/service/UserServiceImp.java
@@ -1,7 +1,21 @@
 package com.socialmedia.be_java_hisp_w18_g08.service;
 
+import com.socialmedia.be_java_hisp_w18_g08.dto.FollowedDTO;
+import com.socialmedia.be_java_hisp_w18_g08.repository.IUserRepository;
+import com.socialmedia.be_java_hisp_w18_g08.repository.UserRepositoryImp;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserServiceImp {
+public class UserServiceImp implements IUserService{
+
+    private IUserRepository userRepository;
+
+    public UserServiceImp(UserRepositoryImp repo){
+        this.userRepository = repo;
+    }
+
+    @Override
+    public FollowedDTO getFollowed(Integer userId) {
+        return null;
+    }
 }
