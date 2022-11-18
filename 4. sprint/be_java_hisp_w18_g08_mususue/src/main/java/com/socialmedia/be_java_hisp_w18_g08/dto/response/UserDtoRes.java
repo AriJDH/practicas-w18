2fd3c573380f7b7f357e15897a48c7076d3f3d1c
vamususue <vanessa.vmususue@mercadolibre.com.co>
2022-11-18 10:50:49 -0500

    Creatind new DTO

---
+++ 4. sprint/be_java_hisp_w18_g08_mususue/src/main/java/com/socialmedia/be_java_hisp_w18_g08/dto/response/UserDtoRes.java
@@ -0,0 +1,23 @@
+package com.socialmedia.be_java_hisp_w18_g08.dto.response;
+
+import com.socialmedia.be_java_hisp_w18_g08.entity.Post;
+import com.socialmedia.be_java_hisp_w18_g08.entity.Seller;
+import com.socialmedia.be_java_hisp_w18_g08.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class UserDtoRes {
+    private Integer user_id;
+    private String user_name;
+    private List<Seller> followed;
+    private List<Post> posts;
+    private List<User> followers;
+}
