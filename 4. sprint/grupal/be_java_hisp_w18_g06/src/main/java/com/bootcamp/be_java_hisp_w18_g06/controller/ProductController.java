54dcaf23f8a37dc5fdeca13a666847ff7e1352a6
Angel Fernandez <angel.afernandez@mercadolibre.com>
2022-11-18 10:11:36 -0300

    merge US-0005 with US-0006 US-0001 US-0007

--- 4. sprint/grupal/be_java_hisp_w18_g06/src/main/java/com/bootcamp/be_java_hisp_w18_g06/controller/ProductController.java
+++ 4. sprint/grupal/be_java_hisp_w18_g06/src/main/java/com/bootcamp/be_java_hisp_w18_g06/controller/ProductController.java
@@ -6,8 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,9 +26,6 @@ public class ProductController {
 	}
 
 
-
-    @Autowired
-    private IProductService productService;
 
     @PostMapping("/post")
     public ResponseEntity<?> save(PostDTO postDTO){
