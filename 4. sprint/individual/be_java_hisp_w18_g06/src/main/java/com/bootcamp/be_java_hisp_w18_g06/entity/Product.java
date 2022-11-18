298619dc71828c850dd5653fe12004cdada71015
Johanna Santos <johanna.santos@mercadolibre.com>
2022-11-18 16:11:29 -0300

    inicio requerimiento individual

---
+++ 4. sprint/individual/be_java_hisp_w18_g06/src/main/java/com/bootcamp/be_java_hisp_w18_g06/entity/Product.java
@@ -0,0 +1,17 @@
+package com.bootcamp.be_java_hisp_w18_g06.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Product {
+    private int product_id;
+    private String product_name;
+    private String type;
+    private String brand;
+    private String color;
+    private String notes;
+}
---
+++ 4. sprint/individual/be_java_hisp_w18_g06/src/main/java/com/bootcamp/be_java_hisp_w18_g06/repository/IUserRepository.java
@@ -0,0 +1,16 @@
+package com.bootcamp.be_java_hisp_w18_g06.repository;
+
+import com.bootcamp.be_java_hisp_w18_g06.entity.Post;
+import com.bootcamp.be_java_hisp_w18_g06.entity.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IUserRepository {
+
+    void createPost(Post post);
+    Optional<User> findUserById(int id);
+    Optional<User> findUserInList(List<User> list, int idUserInList);
+    User getUser(int userId);
+    void updateUsers(User userUpdate);
+}
