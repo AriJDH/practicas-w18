5330925846925b37528074693fede15cfa7b45d2
mhaller <melisa.haller@mercadolibre.com>
2022-11-18 14:31:06 -0300

    Sprint 1 Grupal

---
+++ 4. sprint/be_java_hisp_w18_g10/src/main/java/com/dh/be_java_hisp_w18_g10/dto/response/ErrorDTOres.java
@@ -0,0 +1,19 @@
+package com.dh.be_java_hisp_w18_g10.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorDTOres {
+    private String message;
+    private int code;
+}
+
+// JSON REPRESENTATION
+// {
+//  "message": "",
+//  "code": 0
+//}
---
+++ 4. sprint/be_java_hisp_w18_g10/src/main/java/com/dh/be_java_hisp_w18_g10/exception/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package com.dh.be_java_hisp_w18_g10.exception;
+
+public class UserNotFoundException extends RuntimeException{
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
