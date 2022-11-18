f0f3d49c9b16d83899ac9df91dedfdedc6d90630
ximeMeli <ximenaanabela.milla@mercadolibre.com>
2022-11-18 16:28:19 -0300

    Group folder

---
+++ 4. sprint/be_java_hisp_w18_g08/src/main/java/com/socialmedia/be_java_hisp_w18_g08/dto/response/SellerFollowersCountDto.java
@@ -0,0 +1,17 @@
+package com.socialmedia.be_java_hisp_w18_g08.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class SellerFollowersCountDto {
+
+    private Integer user_id;
+    private String user_name;
+    private Integer followers_count;
+}
