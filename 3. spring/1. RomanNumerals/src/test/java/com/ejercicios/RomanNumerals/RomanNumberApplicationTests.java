8f067e6a480ab4062de3d724a2ff315e6cd699f0
mhaller <melisa.haller@mercadolibre.com>
2022-11-21 11:34:04 -0300

    modificaciones en el nombre de la carpeta

---
+++ 3. spring/1. RomanNumerals/src/test/java/com/ejercicios/RomanNumerals/RomanNumberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejercicios.RomanNumerals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RomanNumberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
