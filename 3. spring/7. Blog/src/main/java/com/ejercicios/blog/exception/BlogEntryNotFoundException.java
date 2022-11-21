7437ccab8654756c3d2edf8f1d02c355e2250bf0
MeLiHaller <melisa.haller@mercadolibre.com>
2022-11-21 17:06:49 -0300

    Delete 3. spring/7. Blog directory
    Subido equivocadamente

--- 3. spring/7. Blog/src/main/java/com/ejercicios/blog/exception/BlogEntryNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.ejercicios.blog.exception;
-
-public class BlogEntryNotFoundException extends RuntimeException {
-    public BlogEntryNotFoundException(String message) {
-        super(message);
-    }
-}
