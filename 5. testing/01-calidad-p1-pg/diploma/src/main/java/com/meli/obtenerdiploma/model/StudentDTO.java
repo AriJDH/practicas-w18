228f2752d9482e7c7b93b80094c035373d99d378
aagaard-meli <martin.aagaard@mercadolibre.com>
2022-11-22 12:23:11 -0300

    5-testing 01-calidad-p1-pg diploma fix

--- 5. testing/01-calidad-p1-pg/diploma/src/main/java/com/meli/obtenerdiploma/model/StudentDTO.java
+++ 5. testing/01-calidad-p1-pg/diploma/src/main/java/com/meli/obtenerdiploma/model/StudentDTO.java
@@ -3,6 +3,8 @@ package com.meli.obtenerdiploma.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
@@ -10,12 +12,12 @@ import java.util.List;
 
 @Getter @Setter
 public class StudentDTO {
-    @NotEmpty(message = "El nombre del alumno no puede estar vacío.")
+    @NotBlank(message = "El nombre del alumno no puede estar vacío.")
     @Pattern(regexp = "^[A-Z].*", message = "El nombre del alumno debe comenzar con mayúscula.")
     @Size(max = 50, message = "La longitud del nombre no puede superar los 50 caracteres.")
     String studentName;
     String message;
     Double averageScore;
     @NotEmpty(message = "La lista no puede ser vacía.")
-    List<SubjectDTO> subjects;
+    List<@Valid SubjectDTO> subjects;
 }
--- 5. testing/01-calidad-p1-pg/diploma/src/main/java/com/meli/obtenerdiploma/model/SubjectDTO.java
+++ 5. testing/01-calidad-p1-pg/diploma/src/main/java/com/meli/obtenerdiploma/model/SubjectDTO.java
@@ -8,11 +8,11 @@ import javax.validation.constraints.*;
 
 @Getter @Setter
 public class SubjectDTO {
-    @NotEmpty(message = "El nombre de la materia no puede estar vacío.")
+    @NotBlank(message = "El nombre de la materia no puede estar vacío.")
     @Pattern(regexp = "^[A-Z].*", message = "El nombre de la materia debe comenzar con mayúscula.")
     @Size(max = 30, message = "La longitud del nombre no puede superar los 30 caracteres.")
     String name;
-    @NotEmpty(message = "La nota no puede estar vacía.")
+    @NotNull(message = "La nota no puede estar vacía.")
     @Range(min=0, max=10, message = "La mínima nota es 0.0. La máxima nota es 10.0.")
     Double score;
 }
