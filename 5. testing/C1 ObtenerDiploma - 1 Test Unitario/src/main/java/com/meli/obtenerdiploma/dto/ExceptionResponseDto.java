8f6e616d76ef884fa3dc78559848bdae47198c70
icingolani <ivanna.cingolani@mercadolibre.com>
2022-11-23 14:56:11 -0300

    Add List messages errors, status and timestamp -  exception

--- 5. testing/C1 ObtenerDiploma - 1 Test Unitario/src/main/java/com/meli/obtenerdiploma/dto/ExceptionResponseDto.java
+++ 5. testing/C1 ObtenerDiploma - 1 Test Unitario/src/main/java/com/meli/obtenerdiploma/dto/ExceptionResponseDto.java
@@ -5,9 +5,18 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 public class ExceptionResponseDto {
-    private String message;
+    private List<String> messages;
+    //private String message;
+    private int status;
+    private LocalDate timeStamp;
+
+    public ExceptionResponseDto(String message) {
+    }
 }
--- 5. testing/C1 ObtenerDiploma - 1 Test Unitario/src/main/java/com/meli/obtenerdiploma/exceptions/GlobalExceptionHandler.java
+++ 5. testing/C1 ObtenerDiploma - 1 Test Unitario/src/main/java/com/meli/obtenerdiploma/exceptions/GlobalExceptionHandler.java
@@ -9,29 +9,42 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @ControllerAdvice(annotations = RestController.class)
 public class GlobalExceptionHandler {
 
+/*
     @ExceptionHandler(BadRequestException.class)
     public ResponseEntity<ExceptionResponseDto> badRequestException(BadRequestException e) {
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                 .body(new ExceptionResponseDto(e.getMessage()));
     }
+*/
 
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
+/*    @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ExceptionResponseDto> emptyException(MethodArgumentNotValidException e) {
 
         return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE)  //TODO Chequear status
                 .body(new ExceptionResponseDto(e.getBindingResult().getFieldError().getDefaultMessage()));
+    }   */
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ExceptionResponseDto> emptyException(MethodArgumentNotValidException e) {
+        List<String> messages = e.getFieldErrors().stream().map(s -> s.getDefaultMessage()).collect(Collectors.toList());
+        return new ResponseEntity<>(new ExceptionResponseDto(messages, 400, LocalDate.now()), HttpStatus.BAD_REQUEST);
     }
 
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public ResponseEntity<ExceptionResponseDto> notFoundException(HttpMessageNotReadableException e) {
-
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST) //TODO Chequear status
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                 .body(new ExceptionResponseDto(e.getMessage()));
     }
+
+
 }
