2e9b22a38c1172c0bd82794eaffd92f2625412e4
Janeth Martinez Lucio <janeth.martinez@mercadolibre.com.mx>
2022-11-23 12:11:59 -0600

    fit controller

--- 5. testing/ObtenerDiploma/src/main/java/com/meli/obtenerdiploma/model/StudentDTO.java
+++ 5. testing/ObtenerDiploma/src/main/java/com/meli/obtenerdiploma/model/StudentDTO.java
@@ -2,18 +2,17 @@ package com.meli.obtenerdiploma.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
+import lombok.NonNull;
 import lombok.Setter;
 
 import javax.validation.Valid;
-import javax.validation.constraints.Max;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Pattern;
+import javax.validation.constraints.*;
 import java.util.List;
 
 @Getter @Setter
 public class StudentDTO {
     @NotEmpty(message = "El campo studentName no peude ser vacio")
-    @Max(value  = 50 , message = "La longitud maxima permitida es de 50 caracteres" )
+    @Size(min = 5, max=50 , message = "El valor debe ser menor a 50 caracteres")
     @Pattern(regexp="(^[A-Z])(\\w||\\s)*", message = "El nombre debe comenzar en mayuscula")
     String studentName;
     String message;
--- 5. testing/ObtenerDiploma/src/main/java/com/meli/obtenerdiploma/model/SubjectDTO.java
+++ 5. testing/ObtenerDiploma/src/main/java/com/meli/obtenerdiploma/model/SubjectDTO.java
@@ -3,18 +3,17 @@ package com.meli.obtenerdiploma.model;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.validation.constraints.Max;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
 
-@Getter @Setter
+@Getter
+@Setter
 public class SubjectDTO {
     @NotEmpty(message = "El campo name no puede ser vacio")
-    @Max(value  = 30 , message = "La longitud maxima permitida es de 30 caracteres" )
-    @Pattern(regexp="(^[A-Z])(\\w||\\s)*", message = "El nombre debe comenzar en mayuscula")
+    @Size(min = 0, max = 30, message = "La longitud maxima permitida es de 30 caracteres")
+    @Pattern(regexp = "(^[A-Z])(\\w||\\s)*", message = "El nombre debe comenzar en mayuscula")
     String name;
-    @NotEmpty(message = "El campo name no puede ser vacio")
-@Size(min = 0, max = 10, message = "El valor minimo es de 0.0 y maximo de 10")
+    @Min(value = 0, message = "El valor minimo es de 0.0 ")
+    @Max(value =  10, message = "El valor no puede ser mayor a 10.0")
+    @NotNull(message = "El campo score no puede ser vacio")
     Double score;
 }
