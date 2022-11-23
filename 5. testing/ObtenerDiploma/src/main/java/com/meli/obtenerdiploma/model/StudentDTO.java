c077656c340b04f4abecb07cf82a4d326f7211e4
Janeth Martinez Lucio <janeth.martinez@mercadolibre.com.mx>
2022-11-23 15:16:01 -0600

    Adjusted to receive accents

--- 5. testing/ObtenerDiploma/src/main/java/com/meli/obtenerdiploma/model/StudentDTO.java
+++ 5. testing/ObtenerDiploma/src/main/java/com/meli/obtenerdiploma/model/StudentDTO.java
@@ -1,8 +1,6 @@
 package com.meli.obtenerdiploma.model;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
-import lombok.NonNull;
 import lombok.Setter;
 
 import javax.validation.Valid;
@@ -11,9 +9,9 @@ import java.util.List;
 
 @Getter @Setter
 public class StudentDTO {
-    @NotEmpty(message = "El campo studentName no peude ser vacio")
+    @NotEmpty(message = "El campo nombre no puede ser vacio")
     @Size(min = 5, max=50 , message = "El valor debe ser menor a 50 caracteres")
-    @Pattern(regexp="(^[A-Z])(\\w||\\s)*", message = "El nombre debe comenzar en mayuscula")
+    @Pattern(regexp="([A-Z]|[0-9])[\\s|[0-9]|A-Z|a-z|ñ|ó|í|á|é|ú|Á|Ó|É|Í|Ú]*$", message = "El nombre del estudiante debe comenzar con mayúscula.")
     String studentName;
     String message;
     Double averageScore;
--- 5. testing/ObtenerDiploma/src/main/java/com/meli/obtenerdiploma/model/SubjectDTO.java
+++ 5. testing/ObtenerDiploma/src/main/java/com/meli/obtenerdiploma/model/SubjectDTO.java
@@ -8,9 +8,9 @@ import javax.validation.constraints.*;
 @Getter
 @Setter
 public class SubjectDTO {
-    @NotEmpty(message = "El campo name no puede ser vacio")
+    @NotEmpty(message = "El campo nombre no puede ser vacio")
     @Size(min = 0, max = 30, message = "La longitud maxima permitida es de 30 caracteres")
-    @Pattern(regexp = "(^[A-Z])(\\w||\\s)*", message = "El nombre debe comenzar en mayuscula")
+    @Pattern(regexp="([A-Z]|[0-9])[\\s|[0-9]|A-Z|a-z|ñ|ó|í|á|é|ú|Á|Ó|É|Í|Ú]*$", message = "El nombre del estudiante debe comenzar con mayúscula.")
     String name;
     @Min(value = 0, message = "El valor minimo es de 0.0 ")
     @Max(value =  10, message = "El valor no puede ser mayor a 10.0")
