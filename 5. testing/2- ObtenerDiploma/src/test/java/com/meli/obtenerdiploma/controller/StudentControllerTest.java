bb9cf8da61e5434163f60c61c3604f61550bf4b2
korec_micaela <micaelakorec@mercadolibre.com>
2022-11-24 11:40:55 -0300

    Testing_2 - Test unitarios OK en studentController: listStudents

--- 5. testing/2- ObtenerDiploma/src/test/java/com/meli/obtenerdiploma/controller/StudentControllerTest.java
+++ 5. testing/2- ObtenerDiploma/src/test/java/com/meli/obtenerdiploma/controller/StudentControllerTest.java
@@ -3,14 +3,19 @@ package com.meli.obtenerdiploma.controller;
 import com.meli.obtenerdiploma.model.StudentDTO;
 import com.meli.obtenerdiploma.model.SubjectDTO;
 import com.meli.obtenerdiploma.service.IStudentService;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.platform.commons.util.CollectionUtils;
+
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -25,6 +30,7 @@ class StudentControllerTest {
     @InjectMocks
     StudentController studentController;
 
+    @DisplayName("US0001 - registerStudent OK")
     @Test
     void registerStudent() {
         //ARRANGE
@@ -39,6 +45,7 @@ class StudentControllerTest {
         verify(studentService, atLeastOnce()).create(student);
     }
 
+    @DisplayName("US0002 - getStudent OK")
     @Test
     void getStudent() {
         //ARRANGE
@@ -56,6 +63,7 @@ class StudentControllerTest {
         assertEquals(student, studentResult);
     }
 
+    @DisplayName("US0003 - modifyStudent OK")
     @Test
     void modifyStudent() {
         //ARRANGE
@@ -67,9 +75,10 @@ class StudentControllerTest {
         studentController.modifyStudent(student);
 
         //ASSERT
-       verify(studentService, atLeastOnce()).update(student);
+        verify(studentService, atLeastOnce()).update(student);
     }
 
+    @DisplayName("US0004 - removeStudent OK")
     @Test
     void removeStudent() {
         //ARRANGE
@@ -84,8 +93,31 @@ class StudentControllerTest {
         verify(studentService, atLeastOnce()).delete(id);
     }
 
+
+    @DisplayName("US0005 - listStudents OK")
     @Test
     void listStudents() {
+        //ARRANGE
+        SubjectDTO materia1 = new SubjectDTO("Lengua", 6.6);
+        StudentDTO student1 = new StudentDTO(123345L, "Student", "Student", 6.6, Arrays.asList(materia1));
+        SubjectDTO materia2 = new SubjectDTO("Lengua", 6.6);
+        StudentDTO student2 = new StudentDTO(45634L, "Student", "Student", 6.6, Arrays.asList(materia2));
+        SubjectDTO materia3 = new SubjectDTO("Lengua", 6.6);
+        StudentDTO student3 = new StudentDTO(2345678L, "Student", "Student", 6.6, Arrays.asList(materia3));
 
+        Set<StudentDTO> listaMock = new HashSet<>();
+        listaMock.add(student1);
+        listaMock.add(student2);
+        listaMock.add(student3);
+
+        when(studentService.getAll()).thenReturn(listaMock);
+
+        //ACT
+        Set<StudentDTO> result = studentController.listStudents();
+
+        //ASSERT
+        verify(studentService, atLeastOnce()).getAll();
+
+        //assertEquals(listaMock, result);
     }
 }
