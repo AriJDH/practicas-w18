363c88ce64f35c0c31e9432864a4ea814f161880
JonaaCaucota <jonathan.caucota@mercadolibre.com>
2022-11-24 14:41:44 -0300

    Terminado test unitario de los repository

---
+++ 5. testing/LiveCodingTestTerminadoGrupalmente/src/test/java/com/example/livecodingtest/controller/IntegrationTest.java
@@ -0,0 +1,34 @@
+package com.example.livecodingtest.integration;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class IntegrationTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+
+    @Test
+    void intTest() throws Exception {
+        MvcResult mvcResult = this.mockMvc.perform(MockMvcRequestBuilders.get("/getAllUsers"))
+                .andDo(print()).andExpect(status().isOk())
+                .andExpect(content().contentType("application/json"))
+                .andReturn();
+
+        System.out.println(mvcResult.getResponse().getContentAsString());
+
+    }
+}
