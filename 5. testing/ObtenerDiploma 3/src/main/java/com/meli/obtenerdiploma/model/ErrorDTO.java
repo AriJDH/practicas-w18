95e6b035cfa673ac3ab77b23d100ab9f29c7e182
Sebastián Gines <sebastian.gines@mercadolibre.com>
2022-11-24 11:58:09 -0300

    Agregado de ejercicio liveCoding Restaurant

--- 5. testing/ObtenerDiploma 3/src/main/java/com/meli/obtenerdiploma/model/ErrorDTO.java
+++ 5. testing/ObtenerDiploma 3/src/main/java/com/meli/obtenerdiploma/model/ErrorDTO.java
@@ -1,14 +1,15 @@
 package com.meli.obtenerdiploma.model;
 
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Setter
-@Getter
-@NoArgsConstructor
+@Getter @Setter
 @AllArgsConstructor
-public class CustomException {
-    private String message;
+@NoArgsConstructor
+public class ErrorDTO {
+    private String name;
+    private String description;
 }
--- 5. testing/ObtenerDiploma/src/main/java/com/meli/obtenerdiploma/exception/ExceptionController.java
+++ 5. testing/ObtenerDiploma/src/main/java/com/meli/obtenerdiploma/exception/ExceptionController.java
@@ -1,6 +1,6 @@
 package com.meli.obtenerdiploma.exception;
 
-import com.meli.obtenerdiploma.model.CustomException;
+import com.meli.obtenerdiploma.model.ErrorDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
@@ -8,19 +8,25 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @ControllerAdvice
 public class ExceptionController {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<CustomException> handleValidationExceptions(MethodArgumentNotValidException ex){
-        CustomException customException = new CustomException(ex.getBindingResult().getFieldError().getDefaultMessage());
-        return new ResponseEntity<>(customException, HttpStatus.BAD_REQUEST);
+    public ResponseEntity<?> handleValidationExceptions(MethodArgumentNotValidException ex){
+        List<ErrorDTO> errores = ex.getBindingResult().getFieldErrors().stream()
+                .map(error -> new ErrorDTO(error.getDefaultMessage()))
+                .distinct()
+                .collect(Collectors.toList());
+        return new ResponseEntity<>(errores, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
-    public ResponseEntity<CustomException> handleValidationExceptions(HttpMessageNotReadableException ex){
-        CustomException customException = new CustomException(ex.getMessage());
-        return new ResponseEntity<>(customException, HttpStatus.BAD_REQUEST);
+    public ResponseEntity<?> handleValidationExceptions(HttpMessageNotReadableException ex){
+        ErrorDTO errorDTO = new ErrorDTO(ex.getMessage());
+        return new ResponseEntity<>(errorDTO, HttpStatus.BAD_REQUEST);
     }
 
 }
