c05573e4a20cb095dd382ef53f21532c0c432802
Angel Fernandez <angel.afernandez@mercadolibre.com>
2022-11-25 16:54:47 -0300

    sprint2 dream

---
+++ 6. sprint II/be_java_hisp_w18_g06/socialMeli2/be_java_hisp_w18_g06/src/main/java/com/bootcamp/be_java_hisp_w18_g06/entity/Post.java
@@ -0,0 +1,29 @@
+package com.bootcamp.be_java_hisp_w18_g06.entity;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@Data
+public class Post {
+    private int post_id=0;
+    private int user_id;
+    @JsonFormat(pattern = "dd-MM-yyyy")
+    private LocalDate date;
+    private Product product;
+    private String category;
+    private double price;
+
+    public Post( int user_id, LocalDate date, Product product, String category, double price) {
+        this.post_id += 1;
+        this.user_id = user_id;
+        this.date = date;
+        this.product = product;
+        this.category = category;
+        this.price = price;
+    }
+}
---
+++ 6. sprint II/be_java_hisp_w18_g06/socialMeli2/be_java_hisp_w18_g06/src/main/java/com/bootcamp/be_java_hisp_w18_g06/entity/User.java
@@ -0,0 +1,25 @@
+package com.bootcamp.be_java_hisp_w18_g06.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
+
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Getter
+@Setter
+public class User {
+    private int user_id;
+    private String user_name;
+    @JsonIgnore
+    private List<User> followers;
+    private List<User> followed;
+    private List<Post> posts;
+    
+    public User(int user_id, String user_name) {
+        this.user_id = user_id;
+        this.user_name = user_name;
+    }
+}
