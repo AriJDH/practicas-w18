e60f1357f28ec7aa6f8fddbffd5c9a7fa68e3abd
ximeMeli <ximenaanabela.milla@mercadolibre.com>
2022-11-25 17:16:53 -0300

    Agrega carpeta src borrada

---
+++ 4. sprint/be_java_hisp_w18_g08/src/main/java/com/socialmedia/be_java_hisp_w18_g08/exception/NotFoundUserException.java
@@ -0,0 +1,8 @@
+package com.socialmedia.be_java_hisp_w18_g08.exception;
+
+public class NotFoundUserException extends RuntimeException {
+
+    public NotFoundUserException(String msg) {
+        super(msg);
+    }
+}
