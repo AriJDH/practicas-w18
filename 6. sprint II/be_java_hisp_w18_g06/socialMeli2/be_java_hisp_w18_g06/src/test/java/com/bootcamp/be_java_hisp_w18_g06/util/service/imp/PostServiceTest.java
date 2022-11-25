c43c7146ffc6d7d7b8386127f15589d5e08010b5
Angel Fernandez <angel.afernandez@mercadolibre.com>
2022-11-25 18:14:17 -0300

    test structure

---
+++ 6. sprint II/be_java_hisp_w18_g06/socialMeli2/be_java_hisp_w18_g06/src/test/java/com/bootcamp/be_java_hisp_w18_g06/util/service/imp/PostServiceTest.java
@@ -0,0 +1,44 @@
+package com.bootcamp.be_java_hisp_w18_g06.util.service.imp;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PostServiceTest {
+
+    //nomenclatura del nombre:
+    // nombre del método + resultado esperado + test
+    //ej: savePostOkTest()
+
+
+    //US00006
+    @Nested
+    class T0008{
+        @Test
+        void findAllPostsByUser() {
+        }
+
+    }
+    //US00009
+    @Nested
+    class T0005_T0006{
+        @Test
+        void sortedByAscAndDesc() {
+        }
+
+    }
+
+    //US00009
+    @Nested
+    class T0005{
+        @Test
+        void sortedByAscAndDesc() {
+        }
+
+    }
+
+
+
+
+}
---
+++ 6. sprint II/be_java_hisp_w18_g06/socialMeli2/be_java_hisp_w18_g06/src/test/java/com/bootcamp/be_java_hisp_w18_g06/util/service/imp/UserServiceTest.java
@@ -0,0 +1,49 @@
+package com.bootcamp.be_java_hisp_w18_g06.util.service.imp;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceTest {
+
+
+    //US0001
+    @Nested
+    class T0001{
+        @Test
+        void followUser() {
+        }
+    }
+
+
+    //US0007
+    @Nested
+    class T0002{
+        @Test
+        void unfollowUser() {
+        }
+    }
+    //US-0008
+    @Nested
+    class T0003{
+        @Test
+        void getFollowedList() {
+        }
+    }
+    //US-0008
+    @Nested
+    class T0004{
+        @Test
+        void getFollowedList() {
+        }
+    }
+    //US0002
+    @Nested
+    class T0007{
+        @Test
+        void getFollowersList() {
+        }
+    }
+
+}
