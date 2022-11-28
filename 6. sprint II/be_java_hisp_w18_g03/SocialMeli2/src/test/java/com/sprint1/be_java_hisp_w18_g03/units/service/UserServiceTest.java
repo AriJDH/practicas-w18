9d9c0ac88f2be9866daf68b37a3769afa896b1f7
iMrJulianMELI <julian.rincon@mercadolibre.com.co>
2022-11-28 10:59:01 -0500

    T0002 Complete

--- 6. sprint II/be_java_hisp_w18_g03/SocialMeli2/src/test/java/com/sprint1/be_java_hisp_w18_g03/units/service/UserServiceTest.java
+++ 6. sprint II/be_java_hisp_w18_g03/SocialMeli2/src/test/java/com/sprint1/be_java_hisp_w18_g03/units/service/UserServiceTest.java
@@ -5,8 +5,6 @@ import com.sprint1.be_java_hisp_w18_g03.Repository.IUserRepository;
 import com.sprint1.be_java_hisp_w18_g03.dto.response.ResponseDTO;
 import com.sprint1.be_java_hisp_w18_g03.entity.Post;
 import com.sprint1.be_java_hisp_w18_g03.exception.NoFoundException;
-import com.sprint1.be_java_hisp_w18_g03.exception.ParamException;
-import com.sprint1.be_java_hisp_w18_g03.service.IPostService;
 import com.sprint1.be_java_hisp_w18_g03.service.UserServiceImp;
 import com.sprint1.be_java_hisp_w18_g03.entity.User;
 import com.sprint1.be_java_hisp_w18_g03.utils.PostFactor;
@@ -16,7 +14,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
@@ -38,7 +35,7 @@ public class UserServiceTest {
     UserServiceImp userServiceImp;
 
     @Test
-    @DisplayName("T-00001 Verificar que el usuario a seguir exista. (US-0001) :D")
+    @DisplayName("T-0001 Verificar que el usuario a seguir exista. (US-0001) :D")
     public void verifyFollowTestOk(){
         //Arrange
         ResponseDTO expectedDto = new ResponseDTO("All ok", 200);
@@ -48,12 +45,15 @@ public class UserServiceTest {
         Post post = PostFactor.getPost(2, userToFollow);
         List<Post> posts = new ArrayList<>();
         posts.add(post);
+
         //Mock
         when(userRepository.findById(userFollower.getUserId())).thenReturn(userFollower);
         when(userRepository.findById(userToFollow.getUserId())).thenReturn(userToFollow);
         when(postRepository.findByUser(userToFollow.getUserId())).thenReturn(posts);
+
         //Act
         ResponseDTO resultDto = userServiceImp.follow(1, 2);
+
         //Assert
         Assertions.assertAll(
                 () -> Assertions.assertEquals(expectedDto.getMessage(), resultDto.getMessage()),
@@ -62,22 +62,60 @@ public class UserServiceTest {
     }
 
     @Test
-    @DisplayName("T-00001 Verificar que el usuario a seguir exista. (US-0001) D: no existe usuario a seguir")
+    @DisplayName("T-0001 Verificar que el usuario a seguir exista. (US-0001) D: no existe usuario a seguir")
     public void verifyFollowTestUserToFollowNotFound(){
         //Arrange
         User userFollower = getUser(1, "Juan");
         User userToFollow = getUser(2, "Federico");
-        Post post = PostFactor.getPost(2, userToFollow);
-        List<Post> posts = new ArrayList<>();
-        posts.add(post);
 
         //Mock
         when(userRepository.findById(userFollower.getUserId())).thenReturn(userFollower);
         when(userRepository.findById(userToFollow.getUserId())).thenReturn(null);
-        /*when(postRepository.findByUser(userToFollow.getUserId())).thenReturn(posts);*/
 
         //Act & Assert
         Assertions.assertThrows(NoFoundException.class, () -> userServiceImp.follow(1, 2));
+    }
+
+    @Test
+    @DisplayName("T-0002 Verificar que el usuario a dejar de seguir exista. (US-0001) :D")
+    public void verifyUnfollowTestOk(){
+        //Arrange
+        ResponseDTO expectedDto = new ResponseDTO("Ok", 200);
+
+        User userFollower = getUser(1, "Juan");
+        User userToFollow = getUser(2, "Federico");
+        userFollower.getListFollowed().add(userToFollow);
+        userToFollow.getListFollowers().add(userFollower);
+
+        //Mock
+        when(userRepository.findById(userFollower.getUserId())).thenReturn(userFollower);
+        when(userRepository.findById(userToFollow.getUserId())).thenReturn(userToFollow);
+
+        //Act
+        ResponseDTO resultDto = userServiceImp.unfollow(1, 2);
+
+        //Assert
+        Assertions.assertAll(
+                () -> Assertions.assertEquals(expectedDto.getMessage(), resultDto.getMessage()),
+                () -> Assertions.assertEquals(expectedDto.getCode(), resultDto.getCode())
+        );
+    }
+
+    @Test
+    @DisplayName("T-0002 Verificar que el usuario a dejar de seguir exista. (US-0001) D: no existe usuario a unfollow")
+    public void verifyUnfollowTestUserToFollowNotFound(){
+        //Arrange
+        User userFollower = getUser(1, "Juan");
+        User userToUnfollow = getUser(2, "Federico");
+        userFollower.getListFollowed().add(userToUnfollow);
+        userToUnfollow.getListFollowers().add(userFollower);
+
+        //Mock
+        when(userRepository.findById(userFollower.getUserId())).thenReturn(userFollower);
+        when(userRepository.findById(userToUnfollow.getUserId())).thenReturn(null);
+
+        //Act & Assert
+        Assertions.assertThrows(NoFoundException.class, () -> userServiceImp.unfollow(1, 2));
     }
 
 }
