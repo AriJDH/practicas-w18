84333619bd25bcbc77a5b9ddf5b85255656cd6f6
Johanna Santos <johanna.santos@mercadolibre.com>
2022-11-28 15:07:56 -0300

    Add T-0003: unit test

--- 6. sprint II/be_java_hisp_w18_g06/src/test/java/com/bootcamp/be_java_hisp_w18_g06/unit/service/imp/UserServiceTest.java
+++ 6. sprint II/be_java_hisp_w18_g06/src/test/java/com/bootcamp/be_java_hisp_w18_g06/unit/service/imp/UserServiceTest.java
@@ -1,12 +1,33 @@
 package com.bootcamp.be_java_hisp_w18_g06.unit.service.imp;
 
+import com.bootcamp.be_java_hisp_w18_g06.dto.response.UserFollowedListDTO;
+import com.bootcamp.be_java_hisp_w18_g06.entity.User;
+import com.bootcamp.be_java_hisp_w18_g06.exceptions.BadRequestException;
+import com.bootcamp.be_java_hisp_w18_g06.repository.imp.UserRepository;
+import com.bootcamp.be_java_hisp_w18_g06.service.imp.UserService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Optional;
+
+import static com.bootcamp.be_java_hisp_w18_g06.utils.UserFactory.getUserWithFollowedList;
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class UserServiceTest {
 
+    @Mock
+    UserRepository repository;
+    @InjectMocks
+    UserService service;
 
     //US0001
     @Nested
@@ -28,7 +49,21 @@ class UserServiceTest {
     @Nested
     class T0003{
         @Test
-        void getFollowedList() {
+        void getFollowedListOk() {
+            String order = "name_asc";
+            User user = getUserWithFollowedList("user with followed list");
+
+            Mockito.when(repository.findUserById(user.getUser_id())).thenReturn(Optional.of(user));
+            Assertions.assertDoesNotThrow(()->service.getFollowedList(user.getUser_id(), order));
+
+        }
+        @Test
+        void getFollowedListNotOk() {
+            String order = "order not found";
+            User user = getUserWithFollowedList("user with followed list");
+
+            Mockito.when(repository.findUserById(user.getUser_id())).thenReturn(Optional.of(user));
+            Assertions.assertThrows(BadRequestException.class, ()->service.getFollowedList(user.getUser_id(), order));
         }
     }
     //US-0008
