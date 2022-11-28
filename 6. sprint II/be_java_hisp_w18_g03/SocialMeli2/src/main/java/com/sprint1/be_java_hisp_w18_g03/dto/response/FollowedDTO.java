8c1136f271f0fb04547e67b2ffa0046257e47ecd
iMrJulianMELI <julian.rincon@mercadolibre.com.co>
2022-11-28 11:40:29 -0500

    T-0004 complete

--- 6. sprint II/be_java_hisp_w18_g03/SocialMeli2/src/main/java/com/sprint1/be_java_hisp_w18_g03/dto/response/FollowedDTO.java
+++ 6. sprint II/be_java_hisp_w18_g03/SocialMeli2/src/main/java/com/sprint1/be_java_hisp_w18_g03/dto/response/FollowedDTO.java
@@ -1,20 +1,21 @@
 package com.sprint1.be_java_hisp_w18_g03.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
+@Setter
+@EqualsAndHashCode
 public class FollowedDTO {
     @JsonProperty("user_id")
     private Integer userId;
     @JsonProperty("user_name")
     private String userName;
     @JsonProperty("followed")
-    private List<UserDTO> followed;
+    private List<UserDTO> followed = new ArrayList<>();
 }
