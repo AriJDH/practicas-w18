30abfb6a113c01cd9f067f90fe307b5a41b98b17
Angel Fernandez <angel.afernandez@mercadolibre.com>
2022-11-28 14:58:19 -0300

    added T-0005

--- 6. sprint II/be_java_hisp_w18_g06/src/test/java/com/bootcamp/be_java_hisp_w18_g06/unit/service/imp/PostServiceTest.java
+++ 6. sprint II/be_java_hisp_w18_g06/src/test/java/com/bootcamp/be_java_hisp_w18_g06/unit/service/imp/PostServiceTest.java
@@ -1,11 +1,32 @@
 package com.bootcamp.be_java_hisp_w18_g06.unit.service.imp;
 
+import com.bootcamp.be_java_hisp_w18_g06.dto.request.PostDTO;
+import com.bootcamp.be_java_hisp_w18_g06.entity.User;
+import com.bootcamp.be_java_hisp_w18_g06.exceptions.BadRequestException;
+import com.bootcamp.be_java_hisp_w18_g06.repository.IUserRepository;
+import com.bootcamp.be_java_hisp_w18_g06.service.imp.PostService;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
 
+import static com.bootcamp.be_java_hisp_w18_g06.utils.PostFactory.getPost;
+import static com.bootcamp.be_java_hisp_w18_g06.utils.UserFactory.getUserRandom;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
+@SpringBootTest
 class PostServiceTest {
+
+    @Mock
+    private IUserRepository userRepository;
+
+    @InjectMocks
+    private PostService postService;
 
     //nomenclatura del nombre:
     // nombre del método + resultado esperado + test
@@ -22,18 +43,50 @@ class PostServiceTest {
     }
     //US00009
     @Nested
-    class T0005_T0006{
+    class T0005{
         @Test
-        void sortedByAscAndDesc() {
+        void findAllPostsByUserOkTest() {
+            //arrange
+            int id=1;
+            String orderAsc="date_asc";
+            User userToAnalyze=getUserRandom("Romina");
+            User userFollowed=getUserRandom("Damian");
+            userFollowed.setFollowers(Collections.singletonList(userToAnalyze));
+            userFollowed.setPosts(Collections.singletonList(getPost(id)));
+            userToAnalyze.setFollowed(Collections.singletonList(userFollowed));
+            //mock
+            when(userRepository.findUserById(id)).thenReturn(Optional.of(userToAnalyze));
+            //act
+            List<PostDTO> posts=postService.findAllPostsByUser(id,orderAsc);
+            //assert
+            assertTrue(posts.size()!=0);
+        }
+        @Test
+        void findAllPostsByUserExceptionTest() {
+            //arrange
+            int id=1;
+            String orderAsc="BadRequest";
+            User userToAnalyze=getUserRandom("Romina");
+            User userFollowed=getUserRandom("Damian");
+            userFollowed.setFollowers(Collections.singletonList(userToAnalyze));
+            userFollowed.setPosts(Collections.singletonList(getPost(id)));
+            userToAnalyze.setFollowed(Collections.singletonList(userFollowed));
+
+            //mock
+            when(userRepository.findUserById(id)).thenReturn(Optional.of(userToAnalyze));
+            //act-assert
+            assertThrows(BadRequestException.class,()->postService.findAllPostsByUser(id,orderAsc));
+
         }
 
     }
 
     //US00009
     @Nested
-    class T0005{
+    class T0006{
         @Test
-        void sortedByAscAndDesc() {
+        void findAllPostsByUserOkTest() {
+
         }
 
     }
