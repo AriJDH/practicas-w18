05e3a2f5bf2a27c412e7596cf1b6b5a13d1572f1
mailenalsinameli <mailen.alsina@mercadolibre.com>
2022-11-29 12:54:39 -0300

    proyecto individual

---
+++ 6. sprint II/be_java_hisp_w18_g05_alsina/be_java_hisp_w18-g05/src/main/java/com/socialmeli/be_java_hisp_w18g05/dto/response/ErrorValidationDTO.java
@@ -0,0 +1,12 @@
+package com.socialmeli.be_java_hisp_w18g05.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class ErrorValidationDTO {
+    private String message;
+}
