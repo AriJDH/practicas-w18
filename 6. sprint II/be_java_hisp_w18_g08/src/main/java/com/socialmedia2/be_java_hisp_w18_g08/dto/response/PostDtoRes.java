b74af05bc290ef77d56e35dc7a4ad1fc66db0725
fedefpazml <federico.paz@mercadolibre.com>
2022-11-29 10:32:00 -0300

    Add T-0006

--- 6. sprint II/be_java_hisp_w18_g08/src/main/java/com/socialmedia2/be_java_hisp_w18_g08/dto/response/PostDtoRes.java
+++ 6. sprint II/be_java_hisp_w18_g08/src/main/java/com/socialmedia2/be_java_hisp_w18_g08/dto/response/PostDtoRes.java
@@ -1,15 +1,14 @@
 package com.socialmedia2.be_java_hisp_w18_g08.dto.response;
 
 import com.socialmedia2.be_java_hisp_w18_g08.dto.PostDto;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.List;
 
 @NoArgsConstructor
 @Getter
 @Setter
+@EqualsAndHashCode
 public class PostDtoRes {
     private Integer user_id;
     private List<PostDto> posts;
