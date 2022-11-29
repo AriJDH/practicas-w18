6a27ba2226e56b27526925e17fe7caca6f454f5e
dacabral <david.cabral@mercadolibre.com>
2022-11-29 12:19:14 -0300

    Added the SocialMeli2 project group base

---
+++ 6. sprint II/SocialMeli2/be_java_hisp_w18_g10_Cabral/src/main/java/com/dh/be_java_hisp_w18_g10/exception/UserGenericException.java
@@ -0,0 +1,7 @@
+package com.dh.be_java_hisp_w18_g10.exception;
+
+public class UserGenericException extends RuntimeException{
+    public UserGenericException(String message) {
+        super(message);
+    }
+}
