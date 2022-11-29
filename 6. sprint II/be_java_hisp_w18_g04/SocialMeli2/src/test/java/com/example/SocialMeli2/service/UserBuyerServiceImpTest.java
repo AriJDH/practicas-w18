9e9b1e8763b2b5f43503d6631a5069ee713c2a91
lucaspadularrosaml <lucas.padularrosa@mercadolibre.com>
2022-11-29 12:59:35 -0300

    Test 007

--- 6. sprint II/be_java_hisp_w18_g04/SocialMeli2/src/test/java/com/example/SocialMeli2/service/UserBuyerServiceImpTest.java
+++ 6. sprint II/be_java_hisp_w18_g04/SocialMeli2/src/test/java/com/example/SocialMeli2/service/UserBuyerServiceImpTest.java
@@ -146,7 +146,8 @@ class UserBuyerServiceImpTest {
     }
 
     @Test
-    void test5() {
+    @DisplayName("T-0007 US-0002. Sort by date DESC")
+    void sortBydateDESC() {
         Integer user_id = 1;
         ArrayList<UserSeller> followed = new ArrayList<>();
         ArrayList<Post> posts = new ArrayList<>();
@@ -160,6 +161,18 @@ class UserBuyerServiceImpTest {
 
         Post post1 = new Post(3, LocalDate.of(2022, 11, 25), new Product(3, "Product 3", "Type 3", "Brand 3", "Color 3", "Notes 3"), 2, 2500.13);
         Post post2 = new Post(7, LocalDate.of(2022, 11, 20), new Product(7, "Product 7", "Type 7", "Brand 7", "Color 7", "Notes 7"), 2, 950.00);
+        List<PostDTORes> postDtoResList= new ArrayList<>();
+
+        PostDTORes postDTORes = new PostDTORes(seller.getUser_id(), post1.getPost_id(), post1.getDate(),
+                new ProductDTORes(post1.getProduct()),
+                post1.getCategory(), post1.getPrice());
+
+        PostDTORes postDTORes2 = new PostDTORes(seller.getUser_id(), post2.getPost_id(), post2.getDate(),
+                new ProductDTORes(post2.getProduct()),
+                post1.getCategory(), post2.getPrice());
+
+        postDtoResList.add(postDTORes);
+        postDtoResList.add(postDTORes2);
 
         posts.add(post1);
         posts.add(post2);
@@ -175,7 +188,58 @@ class UserBuyerServiceImpTest {
 
         PostFollowedByDateDTORes followedByDateDTORes = userBuyerServiceImp.getLastPosts(user_id, "date_desc");
 
-        assertTrue(post1.getDate().equals(followedByDateDTORes.getPosts().get(0).getDate()));
+        //assertTrue(post1.getDate().equals(followedByDateDTORes.getPosts().get(0).getDate()));
+
+        assertTrue(postDtoResList.equals(followedByDateDTORes.getPosts()));
+
+    }
+    @Test
+    @DisplayName("T-0007 US-0002. Sort by date ASC")
+    void sortBydateASC() {
+        Integer user_id = 1;
+        ArrayList<UserSeller> followed = new ArrayList<>();
+        ArrayList<Post> posts = new ArrayList<>();
+        UserBuyer userBuyer = new UserBuyer();
+        userBuyer.setUser_id(1);
+        userBuyer.setUser_name("Josep");
+
+        UserSeller seller = new UserSeller();
+        seller.setUser_name("Kyle");
+        seller.setUser_id(3);
+
+        Post post1 = new Post(3, LocalDate.of(2022, 11, 25), new Product(3, "Product 3", "Type 3", "Brand 3", "Color 3", "Notes 3"), 2, 2500.13);
+        Post post2 = new Post(7, LocalDate.of(2022, 11, 20), new Product(7, "Product 7", "Type 7", "Brand 7", "Color 7", "Notes 7"), 2, 950.00);
+
+        List<PostDTORes> postDtoResList= new ArrayList<>();
+
+        PostDTORes postDTORes = new PostDTORes(seller.getUser_id(), post1.getPost_id(), post1.getDate(),
+                new ProductDTORes(post1.getProduct()),
+                post1.getCategory(), post1.getPrice());
+
+        PostDTORes postDTORes2 = new PostDTORes(seller.getUser_id(), post2.getPost_id(), post2.getDate(),
+                new ProductDTORes(post2.getProduct()),
+                post1.getCategory(), post2.getPrice());
+
+        postDtoResList.add(postDTORes2);
+        postDtoResList.add(postDTORes);
+
+
+        posts.add(post2);
+        posts.add(post1);
+
+        seller.setPosts(posts);
+
+        followed.add(seller);
+        userBuyer.setFollowed(followed);
+
+        // ArrayList Posts
+
+        when(userBuyerRepository.findById(user_id)).thenReturn(Optional.of(userBuyer));
+
+        PostFollowedByDateDTORes followedByDateDTORes = userBuyerServiceImp.getLastPosts(user_id, "date_asc");
+        System.out.println(followedByDateDTORes.getPosts());
+        System.out.println(postDtoResList);
+        assertTrue(postDtoResList.equals(followedByDateDTORes.getPosts()));
 
     }
 }
