6a35aada38d1f864444f4c073ac33557e2ca19d3
Angel Fernandez <angel.afernandez@mercadolibre.com>
2022-11-29 15:10:15 -0300

    added saveExceptionTest()

--- 6. sprint II/be_java_hisp_w18_fernandez_angel/src/test/java/com/bootcamp/be_java_hisp_w18_g06/integration/ProductControllerTest.java
+++ 6. sprint II/be_java_hisp_w18_fernandez_angel/src/test/java/com/bootcamp/be_java_hisp_w18_g06/integration/ProductControllerTest.java
@@ -2,6 +2,7 @@ package com.bootcamp.be_java_hisp_w18_g06.integration;
 
 
 import com.bootcamp.be_java_hisp_w18_g06.dto.request.PostDTO;
+import com.bootcamp.be_java_hisp_w18_g06.dto.response.exception.ExceptionValidResponseDTO;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.junit.jupiter.api.Nested;
@@ -11,10 +12,18 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import static com.bootcamp.be_java_hisp_w18_g06.utils.PostFactory.getPostDto;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -39,6 +48,28 @@ class ProductControllerTest {
                     .andDo(print())
                     .andExpect(status().isOk());
         }
+       @Test
+       void saveExceptionTest() throws Exception {
+          List<String> messages=new ArrayList<>();
+          messages.add("Id must be greater than 0");
+          ExceptionValidResponseDTO validResponseDTO=new ExceptionValidResponseDTO(messages,400, LocalDate.now());
+
+          PostDTO requestPostDto=getPostDto(-2);
+
+
+
+          String payload= new ObjectMapper().registerModule(new JavaTimeModule()).writeValueAsString(requestPostDto);
+          String validDtoJson= new ObjectMapper().registerModule(new JavaTimeModule()).writeValueAsString(validResponseDTO);
+
+         MvcResult result= mockMvc.perform(MockMvcRequestBuilders.post("/products/post")
+                  .contentType(MediaType.APPLICATION_JSON)
+                  .content(payload))
+                  .andDo(print())
+                  .andExpect(status().isBadRequest())
+                 .andExpect(content().contentType("application/json"))
+                 .andReturn();
+         assertEquals(validDtoJson,result.getResponse().getContentAsString());
+       }
     }
 
 
