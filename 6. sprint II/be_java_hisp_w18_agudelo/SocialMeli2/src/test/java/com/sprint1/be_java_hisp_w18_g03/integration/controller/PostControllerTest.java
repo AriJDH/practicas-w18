8f29a3e854055c1512035545d7fd433342864bd4
juagudelo <juanpablo.agudelo@mercadolibre.com.co>
2022-11-29 10:23:37 -0500

    modify integration test

--- 6. sprint II/be_java_hisp_w18_agudelo/SocialMeli2/src/test/java/com/sprint1/be_java_hisp_w18_g03/integration/controller/PostControllerTest.java
+++ 6. sprint II/be_java_hisp_w18_agudelo/SocialMeli2/src/test/java/com/sprint1/be_java_hisp_w18_g03/integration/controller/PostControllerTest.java
@@ -1,14 +1,9 @@
 package com.sprint1.be_java_hisp_w18_g03.integration.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.sprint1.be_java_hisp_w18_g03.dto.request.RequestPostDTO;
-import com.sprint1.be_java_hisp_w18_g03.dto.response.ResponseDTO;
-import com.sprint1.be_java_hisp_w18_g03.dto.response.ResponsePostDTO;
-import com.sprint1.be_java_hisp_w18_g03.dto.response.SellersPostDTO;
-import com.sprint1.be_java_hisp_w18_g03.entity.Category;
 import com.sprint1.be_java_hisp_w18_g03.service.PostServiceImp;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -19,14 +14,8 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -42,39 +31,29 @@ class PostControllerTest {
     private ObjectMapper objectMapper;
 
     @BeforeEach
-    void setUp(){
+    void setUp() {
         this.objectMapper = JsonMapper.builder()
                 .addModule(new JavaTimeModule())
                 .build();
     }
 
     @Test
-    void createPost() throws Exception {//ARRANGE
-        when(postServiceImp.createPost(any())).thenReturn(new ResponseDTO());
-        //ACT
-        RequestPostDTO requestPostDTO = new RequestPostDTO(1, LocalDate.now(),null,1,100d,false,0d);
-
+    void createPost() throws Exception {
+        //ACT & ASSERT
+        RequestPostDTO requestPostDTO = new RequestPostDTO(1, LocalDate.now(), null, 1, 100d, false, 0d);
         mockMvc.perform(
-                    post("/products/post")
-                            .content(objectMapper.writeValueAsString(requestPostDTO))
-                            .contentType(MediaType.APPLICATION_JSON)
+                        post("/products/post")
+                                .content(objectMapper.writeValueAsString(requestPostDTO))
+                                .contentType(MediaType.APPLICATION_JSON)
                 )
                 .andExpect(status().isOk());
     }
 
     @Test
     void getPostSellers() throws Exception {
-        //ARRANGE
-        List<ResponsePostDTO> responsePostDTOList = new ArrayList<>();
-        ResponsePostDTO requestPostDTO = new ResponsePostDTO(1,LocalDate.now(),null,1,100d,1);
-        responsePostDTOList.add(requestPostDTO);
-        SellersPostDTO sellersPostDTO = new SellersPostDTO(1,responsePostDTOList);
-
         //ACT & ASSERT
-        when(postServiceImp.getPostSellers(1,null)).thenReturn(sellersPostDTO);
-
         mockMvc.perform(
-                        get("/products/followed/{userId}/list",1,null)
+                        get("/products/followed/{userId}/list", 1, null)
                 )
                 .andExpect(status().isOk());
     }
--- 6. sprint II/be_java_hisp_w18_agudelo/SocialMeli2/src/test/java/com/sprint1/be_java_hisp_w18_g03/integration/controller/UserControllerIntegrationTest.java
+++ 6. sprint II/be_java_hisp_w18_agudelo/SocialMeli2/src/test/java/com/sprint1/be_java_hisp_w18_g03/integration/controller/UserControllerIntegrationTest.java
@@ -36,65 +36,37 @@ class UserControllerIntegrationTest {
     UserServiceImp userServiceImp;
 
     @Test
-    void followerCount() throws Exception {
-        //ARRANGE
-        FollowerCountDTO followerCountDTO = new FollowerCountDTO(1,"juan",1);
-        when(userServiceImp.followerCount(any())).thenReturn(followerCountDTO);
+    void followerCountNoOk() throws Exception {
         //ACT & ASSERT
-        mockMvc.perform(get("/users/{userId}/followers/count",1))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+        mockMvc.perform(get("/users/{userId}/followers/count",-1))
+                .andExpect(status().isBadRequest());
     }
 
     @Test
     void follow() throws Exception {
-        //ARRANGE
-        ResponseDTO responseDTO = new ResponseDTO();
-        when(userServiceImp.follow(anyInt(),anyInt())).thenReturn(responseDTO);
         //ACT & ASSERT
         mockMvc.perform(post("/users/{userId}/follow/{userIdToFollow}",anyInt(),anyInt()))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+                .andExpect(status().isOk());
     }
 
     @Test
     void getFollowedList() throws Exception {
-        //ARRANGE
-        List<UserDTO> followed = new ArrayList<>();
-        followed.add(new UserDTO(2,"juan"));
-        FollowedDTO followedDTO = new FollowedDTO(1,"",followed);
-
         //ACT & ASSERT
-        when(userServiceImp.getFollowedList(1,null)).thenReturn(followedDTO);
-
          mockMvc.perform(get("/users/{userId}/followed/list",1,null))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+                .andExpect(status().isOk());
     }
 
     @Test
     void unfollow() throws Exception {
-        //ARRANGE
-        ResponseDTO responseDTO = new ResponseDTO();
-        when(userServiceImp.unfollow(anyInt(),anyInt())).thenReturn(responseDTO);
         //ACT & ASSERT
         mockMvc.perform(post("/users/{userId}/unfollow/{userIdToUnfollow}",anyInt(),anyInt()))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+                .andExpect(status().isOk());
     }
 
     @Test
     void getListFollowers() throws Exception {
-        //ARRANGE
-        List<UserDTO> followers = new ArrayList<>();
-        followers.add(new UserDTO(2,"juan"));
-        FollowersDTO followersDTO = new FollowersDTO(1,"",followers);
-
         //ACT & ASSERT
-        when(userServiceImp.getFollowersList(1,null)).thenReturn(followersDTO);
-
         mockMvc.perform(get("/users/{userId}/followers/list",1,null))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+                .andExpect(status().isOk());
     }
 }
