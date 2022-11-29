c260244f2cc24e9407c71c45295b1e21a0ebbd88
mhmeli <milton.herrera@mercadolibre.com>
2022-11-29 11:32:19 -0300

    Sprint II individual inicio

---
+++ 6. sprint II/be_java_hisp_w18_g02_herrera/src/main/java/com/mercadolibre/socialmeli2/dto/ProductDto.java
@@ -0,0 +1,39 @@
+package com.mercadolibre.socialmeli2.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.mercadolibre.socialmeli2.utils.Constants;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class ProductDto {
+    @JsonProperty("product_id")
+    @NotNull(message = Constants.PRODUCT_ID_REQUIRED)
+    @Positive(message = Constants.PRODUCT_ID_POSITIVE)
+    private Integer id;
+    @JsonProperty("product_name")
+    @NotBlank(message= Constants.PRODUCT_NAME_REQUIRED)
+    @Size(max=40, message = Constants.PRODUCT_NAME_MAX_LENGTH_40)
+    @Pattern(regexp = "[\\w|\\s]+", message = Constants.PRODUCT_NAME_SPECIAL_CHARACTERS_NOT_ALLOWED)
+    private String name;
+    @NotBlank(message = Constants.TYPE_REQUIRED)
+    @Size(max=15, message = Constants.TYPE_MAX_LENGTH_15)
+    @Pattern(regexp = "[\\w]+", message = Constants.TYPE_SPECIAL_CHARACTERS_NOT_ALLOWED)
+    private String type;
+    @NotBlank(message = Constants.BRAND_REQUIRED)
+    @Size(max=15, message = Constants.BRAND_MAX_LENGTH_15)
+    @Pattern(regexp = "[\\w|\\s]+", message = Constants.BRAND_SPECIAL_CHARACTERS_NOT_ALLOWED)
+    private String brand;
+    @NotBlank(message = Constants.COLOR_REQUIRED)
+    @Size(max=15, message = Constants.COLOR_MAX_LENGTH_15)
+    @Pattern(regexp = "[\\w]+", message = Constants.COLOR_SPECIAL_CHARACTERS_NOT_ALLOWED)
+    private String color;
+    @Size(max=80, message = Constants.MAX_LENGTH_80)
+    @Pattern(regexp = "[\\w|\\s]+", message = Constants.NOTES_SPECIAL_CHARACTERS_NOT_ALLOWED)
+    private String notes;
+}
---
+++ 6. sprint II/be_java_hisp_w18_g02_herrera/src/main/java/com/mercadolibre/socialmeli2/dto/response/ResponseDto.java
@@ -0,0 +1,35 @@
+package com.mercadolibre.socialmeli2.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+public class ResponseDto {
+    private List<String> messages;
+    private int status;
+    @JsonFormat(pattern="dd-MM-yyyy HH:mm:ss")
+    private LocalDateTime timestamp;
+
+    public ResponseDto(){
+        this.messages = new ArrayList<>();
+        this.timestamp = LocalDateTime.now();
+    }
+
+    public ResponseDto(String message, int status){
+        this.messages = new ArrayList<>();
+        this.messages.add(message);
+        this.status = status;
+        this.timestamp = LocalDateTime.now();
+    }
+
+    public ResponseDto(List<String> messages, int status){
+        this.messages = messages;
+        this.status = status;
+        this.timestamp = LocalDateTime.now();
+    }
+
+}
