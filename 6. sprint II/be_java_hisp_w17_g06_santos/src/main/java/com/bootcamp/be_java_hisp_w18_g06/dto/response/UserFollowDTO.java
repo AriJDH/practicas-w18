e3866b57e31f86a38ca187ff0deadddb87f1e8c6
Johanna Santos <johanna.santos@mercadolibre.com>
2022-11-30 13:07:32 -0300

    Fix name of folder

---
+++ 6. sprint II/be_java_hisp_w17_g06_santos/src/main/java/com/bootcamp/be_java_hisp_w18_g06/dto/response/UserFollowDTO.java
@@ -0,0 +1,9 @@
+package com.bootcamp.be_java_hisp_w18_g06.dto.response;
+
+import lombok.Getter;
+
+@Getter
+public class UserFollowDTO {
+    private Integer user_id;
+    private String user_name;
+}
