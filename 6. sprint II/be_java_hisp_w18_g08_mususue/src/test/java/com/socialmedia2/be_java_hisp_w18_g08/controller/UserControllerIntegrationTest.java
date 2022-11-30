e17a9397faf49251af1716492654e0af840fd80c
vamususue <vanessa.vmususue@mercadolibre.com.co>
2022-11-29 21:45:23 -0500

    Integration test follow ok

---
+++ 6. sprint II/be_java_hisp_w18_g08_mususue/src/test/java/com/socialmedia2/be_java_hisp_w18_g08/controller/UserControllerIntegrationTest.java
@@ -0,0 +1,33 @@
+package com.socialmedia2.be_java_hisp_w18_g08.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UserControllerIntegrationTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void followOkTest()throws Exception{
+        this.mockMvc.perform(MockMvcRequestBuilders.post("/users/{userId}/follow/{userIdToFollow}", 1, 5))
+                .andDo(print()).andExpect(status().isOk())
+                .andExpect(content().contentType("application/json"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("User with id:1 already follow to Seller with id:5"))
+                .andReturn();
+
+    }
+
+}
