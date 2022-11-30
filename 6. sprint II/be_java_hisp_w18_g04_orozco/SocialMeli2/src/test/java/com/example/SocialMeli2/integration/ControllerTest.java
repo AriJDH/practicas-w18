dfc2f1657318e45b05fa681e5749dd2c4c00568c
agusOReina18 <agustina.orozco@mercadolibre.com>
2022-11-30 18:13:54 -0300

    ADD methodGetFollowedTest finished

--- 6. sprint II/be_java_hisp_w18_g04_orozco/SocialMeli2/src/test/java/com/example/SocialMeli2/integration/ControllerTest.java
+++ 6. sprint II/be_java_hisp_w18_g04_orozco/SocialMeli2/src/test/java/com/example/SocialMeli2/integration/ControllerTest.java
@@ -1,6 +1,8 @@
 package com.example.SocialMeli2.integration;
 
 import com.example.SocialMeli2.dto.respose.FollowDTORes;
+import com.example.SocialMeli2.dto.respose.FollowedListDTORes;
+import com.example.SocialMeli2.dto.respose.UserDTORes;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,8 +15,13 @@ import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import java.util.ArrayList;
+import java.util.List;
 
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -36,7 +43,7 @@ public class ControllerTest {
 
 
         //MATCHERS
-        ResultMatcher expectedStatus = MockMvcResultMatchers.status().isOk();
+        ResultMatcher expectedStatus = status().isOk();
         ResultMatcher expectedContentType = MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON);
 
         //REQUEST
@@ -53,5 +60,35 @@ public class ControllerTest {
 
     }
 
+    @Test
+    void methodGetFollowedTest() throws Exception {
+        FollowedListDTORes followedListDTORes = new FollowedListDTORes();
+        followedListDTORes.setUser_id(1);
+        followedListDTORes.setUser_name("Josep");
+        List<UserDTORes> followed = new ArrayList<>();
+        UserDTORes userDTOResA = new UserDTORes(2,"Ethan");
+        followed.add(userDTOResA);
+        followedListDTORes.setFollowed(followed);
+        ObjectMapper objectMapper = new ObjectMapper();
+        String responseJson = objectMapper.writeValueAsString(followedListDTORes);
+
+        ResultMatcher expectedStatus = status().isOk();
+        ResultMatcher expectedContentType = MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON);
+
+        MockHttpServletRequestBuilder response = MockMvcRequestBuilders
+                .get("/users/{client_id}/followed/list",1)
+                .param("order","name_asc")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(responseJson);
+
+        mockMvc
+                .perform(response)
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(expectedContentType)
+                .andExpect(MockMvcResultMatchers.jsonPath("$.user_id").value(1))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.user_name").value("Josep"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.followed", hasSize(3)));
+    }
 
 }
