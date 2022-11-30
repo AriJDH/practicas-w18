b22f1c730c6a81c67cf2c0793694d09a1dc61c24
jootero <jose.otero@mercadolibre.com>
2022-11-30 15:43:57 -0300

    T-0012 test para obtener correctamente seguidos por un usuario

--- 6. sprint II/be_java_hisp_w18_OteroDaniel/src/test/java/com/socialmedia2/be_java_hisp_w18_g08/integrationTest/UserControllerTest.java
+++ 6. sprint II/be_java_hisp_w18_OteroDaniel/src/test/java/com/socialmedia2/be_java_hisp_w18_g08/integrationTest/UserControllerTest.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.socialmedia2.be_java_hisp_w18_g08.repository.PostRepositoryImp;
 import com.socialmedia2.be_java_hisp_w18_g08.repository.UserRepositoryImp;
 import com.socialmedia2.be_java_hisp_w18_g08.utils.Repositories;
+import com.socialmedia2.be_java_hisp_w18_g08.utils.Response;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -32,7 +33,6 @@ public class UserControllerTest {
     private UserRepositoryImp userRepo;
 
 
-
     @BeforeEach
     void setUp() {
         Repositories.resetRepos(postRepo, userRepo);
@@ -40,16 +40,35 @@ public class UserControllerTest {
 
     @Test
     @DisplayName("T-0011 - Un usuario sigue correctamente a otro")
-    void followOk() throws Exception{
+    void followOk() throws Exception {
+        //Arrange
+        String response = Response.getFollowDtoRes(1, 5);
         //Matchers
         ResultMatcher expectedStatus = MockMvcResultMatchers.status().isOk();
         ResultMatcher expectedContentType = MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON);
+        ResultMatcher expectedJson = MockMvcResultMatchers.content().json(response);
         //Request
-        MockHttpServletRequestBuilder requestPayload = MockMvcRequestBuilders.get("/products/followed/1/list");
+        MockHttpServletRequestBuilder requestPayload = MockMvcRequestBuilders.post("/users/1/follow/5");
         //Act & Assert
         mockMvc.perform(requestPayload)
                 .andDo(print())
-                .andExpectAll(expectedStatus, expectedContentType);
+                .andExpectAll(expectedStatus, expectedContentType, expectedJson);
+    }
+
+    @Test
+    @DisplayName("T-0012 - Obtener lista de vendedores seguidos por un usuario correctamente")
+    void getFollowedOk() throws Exception {
+        //Arrange
+        String response = Response.getFollowdDtoRes();
+        //Matchers
+        ResultMatcher expectedStatus = MockMvcResultMatchers.status().isOk();
+        ResultMatcher expectedContentType = MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON);
+        ResultMatcher expectedJson = MockMvcResultMatchers.content().json(response);
+        //Act&Assert
+        mockMvc.perform(MockMvcRequestBuilders.get("/users/{userId}/followed/list", 1))
+                .andDo(print())
+                .andExpectAll(expectedStatus, expectedContentType, expectedJson);
+
     }
 
 }
--- 6. sprint II/be_java_hisp_w18_OteroDaniel/src/test/java/com/socialmedia2/be_java_hisp_w18_g08/utils/Response.java
+++ 6. sprint II/be_java_hisp_w18_OteroDaniel/src/test/java/com/socialmedia2/be_java_hisp_w18_g08/utils/Response.java
@@ -3,6 +3,11 @@ package com.socialmedia2.be_java_hisp_w18_g08.utils;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.socialmedia2.be_java_hisp_w18_g08.dto.response.FollowDtoRes;
+import com.socialmedia2.be_java_hisp_w18_g08.dto.response.FollowedDto;
+import com.socialmedia2.be_java_hisp_w18_g08.dto.response.UserDto;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class Response {
 
@@ -12,5 +17,16 @@ public class Response {
         String msg = "User with id:" + id1 + " already follow to Seller with id:" + id2;
         FollowDtoRes res = new FollowDtoRes(200, msg);
         return objectMapper.writeValueAsString(res);
+    }
+
+    public static String getFollowdDtoRes() throws JsonProcessingException {
+        UserDto u1 = new UserDto(5, "User5");
+        UserDto u2 = new UserDto(6, "User6");
+        UserDto u3 = new UserDto(7, "User7");
+        UserDto u4 = new UserDto(8, "User8");
+        List<UserDto> lista = Arrays.asList(u1, u2, u3, u4);
+        FollowedDto response = new FollowedDto(1, "User1", lista);
+
+        return objectMapper.writeValueAsString(response);
     }
 }
