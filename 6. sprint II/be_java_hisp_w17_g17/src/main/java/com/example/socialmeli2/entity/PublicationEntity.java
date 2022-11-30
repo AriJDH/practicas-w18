1478ada50d4d0b8f99dda63eee86e18a61d1e5be
JonaaCaucota <jonathan.caucota@mercadolibre.com>
2022-11-30 13:01:03 -0300

    SPRINT 2 TERMINADO

---
+++ 6. sprint II/be_java_hisp_w17_g17/src/main/java/com/example/socialmeli2/entity/PublicationEntity.java
@@ -0,0 +1,23 @@
+package com.example.socialmeli2.entity;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PublicationEntity {
+
+    private Integer id;
+    @JsonFormat(pattern = "dd-MM-yyyy")
+    private LocalDate date;
+    private Integer userId;
+    private Integer productId;
+    private Integer category;
+    private Double price;
+
+}
---
+++ 6. sprint II/be_java_hisp_w17_g17/src/main/java/com/example/socialmeli2/service/UserServiceImpl.java
@@ -0,0 +1,142 @@
+package com.example.socialmeli2.service;
+
+import com.example.socialmeli2.dto.response.UserBasicResponse;
+import com.example.socialmeli2.dto.response.UserFollowedListResponse;
+import com.example.socialmeli2.dto.response.UserFollowerCountResponse;
+import com.example.socialmeli2.dto.response.UserFollowersListResponse;
+import com.example.socialmeli2.entity.UserEntity;
+import com.example.socialmeli2.exception.NotFoundException;
+import com.example.socialmeli2.repository.IUserRepository;
+import com.example.socialmeli2.util.ListSorterByOrder;
+import com.example.socialmeli2.util.UserMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements IUserService {
+
+    private final IUserRepository userRepository;
+
+
+    /**
+     * Permite que un usuario comience a seguir a otro usuario. Evalua que ambos usuarios pasados como parametro
+     * existan y que el usuario objetivo sea un vendedor o tenga al menos una publicacion
+     *
+     * @param userId         Usuario que va a comenzar a seguir a otro
+     * @param userIdToFollow Usuario objetivo a seguir
+     * @return un booleano para identifcar si se completó la acción
+     */
+    @Override
+    public boolean followUserSeller(Integer userId, Integer userIdToFollow) {
+
+        boolean exist = false;
+
+        if (!userRepository.exist(userId)) {
+            throw new NotFoundException("No existe usuario con ID: " + userId);
+        }
+
+        if (!userRepository.exist(userIdToFollow)) {
+            throw new NotFoundException("No existe usuario con ID: " + userIdToFollow);
+        }
+        if (userRepository.isSeller(userIdToFollow)) {
+            userRepository.followSeller(userId, userIdToFollow);
+            exist = true;
+        } else {
+            throw new NotFoundException("El usuario con ID: " + userIdToFollow + " no es vendedor");
+        }
+
+        return exist;
+    }
+
+    /**
+     * This method is used to unfollow a seller.
+     *
+     * @param userId           the user ID who wants to unfollow a seller
+     * @param userIdToUnfollow the seller/user ID to unfollow
+     */
+    @Override
+    public boolean unfollowUserSeller(Integer userId, Integer userIdToUnfollow) {
+        boolean unfollowSeller = false;
+        if (!userRepository.exist(userId)) {
+            throw new NotFoundException("No existe usuario con ID: " + userId);
+        }
+        if (!userRepository.exist(userIdToUnfollow)) {
+            throw new NotFoundException("No existe usuario con ID: " + userIdToUnfollow);
+        }
+        if (userRepository.getEntityById(userId).ifFollowerExist(userIdToUnfollow)) {
+            userRepository.unfollowSeller(userId, userIdToUnfollow);
+            unfollowSeller = true;
+        }
+        return unfollowSeller;
+    }
+
+    /**
+     * This method is used to count the numbers of followers of a User
+     *
+     * @param userId the User ID to count his followers
+     * @return The count of followers
+     */
+    @Override
+    public UserFollowerCountResponse getUserFollowersCount(Integer userId) {
+        UserEntity userCount = userRepository.getEntityById(userId);
+        return UserMapper.userEntity2UserFollowerCountResponse(userCount);
+    }
+
+    /**
+     * This method when is called will return a user_id with its user_name
+     * and a list of users followed with user_id and user_name.
+     * If the user don´t follow anyone, the list will be empty
+     *
+     * @param userId enter the id of the user that we will retrieve the list of followers
+     * @param order  Optional. enter the value "name_asc" or "name_desc" to order the list of followed users by user_name
+     * @return returns an UserFollowedListResponse object made of userId, userName and List<>UserResponse
+     */
+    @Override
+    public UserFollowersListResponse getFollowersUsersById(Integer userId, String order) {
+        UserEntity seller = userRepository.getEntityById(userId);
+        List<UserBasicResponse> followersDtos = new ArrayList<>();
+
+
+        for (Integer followerId : seller.getFollowersList()) {
+            UserEntity follower = userRepository.getEntityById(followerId);
+            followersDtos.add(UserMapper.userEntity2UserBasicResponse(follower));
+        }
+
+        if (!order.isEmpty()) {
+            followersDtos = ListSorterByOrder.userSortedByName(followersDtos, order);
+        }
+
+        return UserMapper.userEntity2UserFollowersListResponse(seller, followersDtos);
+    }
+
+    /**
+     * This method when is called will return a user_id with its user_name
+     * and a list of users followed with user_id and user_name.
+     * If the user don´t follow anyone, the list will be empty
+     *
+     * @param id    enter the id of the user that we will retrieve the list of followers
+     * @param order Optional. enter the value "name_asc" or "name_desc" to order the list of followed users by user_name
+     * @return returns an UserFollowedListResponse object made of userId, userName and List<>UserResponse
+     */
+    @Override
+    public UserFollowedListResponse getFollowedUsersById(Integer id, String order) {
+        UserEntity entity = userRepository.getEntityById(id);
+        List<UserBasicResponse> followedList = new ArrayList<>();
+
+        for (Integer integer : entity.getFollowedList()) {
+            followedList.add(UserMapper.userEntity2UserBasicResponse(userRepository.getEntityById(integer)));
+        }
+
+
+        if (!order.isEmpty()) {
+            followedList = ListSorterByOrder.userSortedByName(followedList, order);
+        }
+
+        return UserMapper.userEntity2UserFollowedListResponse(entity, followedList);
+    }
+
+}
