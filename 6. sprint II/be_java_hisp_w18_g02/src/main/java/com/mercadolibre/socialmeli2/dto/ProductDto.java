ba2e02804bd9fb0d52a2bbf858c728f860a2e572
nsanchezdona <nicolas.sanchezdonadini@mercadolibre.com>
2022-11-30 17:01:01 -0300

    Fixed minor issue regarding ProductDto Regex validations

--- 6. sprint II/be_java_hisp_w18_g02/src/main/java/com/mercadolibre/socialmeli2/dto/ProductDto.java
+++ 6. sprint II/be_java_hisp_w18_g02/src/main/java/com/mercadolibre/socialmeli2/dto/ProductDto.java
@@ -19,21 +19,21 @@ public class ProductDto {
     @JsonProperty("product_name")
     @NotBlank(message= Constants.PRODUCT_NAME_REQUIRED)
     @Size(max=40, message = Constants.PRODUCT_NAME_MAX_LENGTH_40)
-    @Pattern(regexp = "[\\w|\\s]+", message = Constants.PRODUCT_NAME_SPECIAL_CHARACTERS_NOT_ALLOWED)
+    @Pattern(regexp = "[\\w|\\s]*", message = Constants.PRODUCT_NAME_SPECIAL_CHARACTERS_NOT_ALLOWED)
     private String name;
     @NotBlank(message = Constants.TYPE_REQUIRED)
     @Size(max=15, message = Constants.TYPE_MAX_LENGTH_15)
-    @Pattern(regexp = "[\\w]+", message = Constants.TYPE_SPECIAL_CHARACTERS_NOT_ALLOWED)
+    @Pattern(regexp = "[\\w]*", message = Constants.TYPE_SPECIAL_CHARACTERS_NOT_ALLOWED)
     private String type;
     @NotBlank(message = Constants.BRAND_REQUIRED)
     @Size(max=15, message = Constants.BRAND_MAX_LENGTH_15)
-    @Pattern(regexp = "[\\w|\\s]+", message = Constants.BRAND_SPECIAL_CHARACTERS_NOT_ALLOWED)
+    @Pattern(regexp = "[\\w|\\s]*", message = Constants.BRAND_SPECIAL_CHARACTERS_NOT_ALLOWED)
     private String brand;
     @NotBlank(message = Constants.COLOR_REQUIRED)
     @Size(max=15, message = Constants.COLOR_MAX_LENGTH_15)
-    @Pattern(regexp = "[\\w]+", message = Constants.COLOR_SPECIAL_CHARACTERS_NOT_ALLOWED)
+    @Pattern(regexp = "[\\w]*", message = Constants.COLOR_SPECIAL_CHARACTERS_NOT_ALLOWED)
     private String color;
     @Size(max=80, message = Constants.MAX_LENGTH_80)
-    @Pattern(regexp = "[\\w|\\s]+", message = Constants.NOTES_SPECIAL_CHARACTERS_NOT_ALLOWED)
+    @Pattern(regexp = "[\\w|\\s]*", message = Constants.NOTES_SPECIAL_CHARACTERS_NOT_ALLOWED)
     private String notes;
 }
