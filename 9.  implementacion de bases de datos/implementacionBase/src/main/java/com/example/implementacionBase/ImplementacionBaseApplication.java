d6153d56b449c0958edb38bca8b75751c5a1561c
Svyatoslav Andriychenko <svyatoslav.andriychenko@mercadolibre.com>
2022-12-06 16:33:57 -0300

    Implementation of BBDD testers exercise

---
+++ 9.  implementacion de bases de datos/implementacionBase/src/main/java/com/example/implementacionBase/ImplementacionBaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.implementacionBase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImplementacionBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImplementacionBaseApplication.class, args);
+	}
+
+}
---
+++ 9.  implementacion de bases de datos/implementacionBase/src/test/java/com/example/implementacionBase/ImplementacionBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.implementacionBase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImplementacionBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
