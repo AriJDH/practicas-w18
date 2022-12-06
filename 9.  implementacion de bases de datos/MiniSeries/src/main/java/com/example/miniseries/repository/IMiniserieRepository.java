ec7caa07a2bcfcae00ce444df001004f44823b4d
nsanchezdona <nicolas.sanchezdonadini@mercadolibre.com>
2022-12-06 12:47:11 -0300

    Practico MiniSeries

--- 9.  implementacion de bases de datos/MiniSeries/src/main/java/com/example/miniseries/repository/IMiniserieRepository.java
+++ 9.  implementacion de bases de datos/MiniSeries/src/main/java/com/example/miniseries/repository/IMiniserieRepository.java
@@ -1,2 +1,9 @@
-package com.example.miniseries.repository;public interface IMiniserieRepository {
+package com.example.miniseries.repository;
+
+import com.example.miniseries.entity.MiniSerie;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IMiniserieRepository extends JpaRepository<MiniSerie, Long> {
 }
