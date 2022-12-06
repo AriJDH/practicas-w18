87be0c5a01949a73a9c6180771034544bc1835d9
aagaard-meli <martin.aagaard@mercadolibre.com>
2022-12-06 12:36:54 -0300

    8-implementacionDb 02-jpaHibernate-p1-vivo MiniSeries (h2)

---
+++ 9.  implementacion de bases de datos/02-jpaHibernate-p1-vivo/MiniSeries/src/main/java/com/water/miniseries/entity/MiniSerie.java
@@ -0,0 +1,21 @@
+package com.water.miniseries.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@Entity
+public class MiniSerie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    Long id;
+    String name;
+    Double rating;
+    int amount_of_awards;
+}
---
+++ 9.  implementacion de bases de datos/02-jpaHibernate-p1-vivo/MiniSeries/src/main/java/com/water/miniseries/repository/IMiniserieRepository.java
@@ -0,0 +1,9 @@
+package com.water.miniseries.repository;
+
+import com.water.miniseries.entity.MiniSerie;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IMiniserieRepository extends JpaRepository<MiniSerie, Long> {
+}
