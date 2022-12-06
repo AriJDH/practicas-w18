7e862da71c7a8d1f6b984acbb22fca2db7dde212
fedefpazml <federico.paz@mercadolibre.com>
2022-12-06 17:45:21 -0300

    Add Test Cases

---
+++ 9.  implementacion de bases de datos/TestCasesJpa/src/main/java/com/meli/testcasesjpa/exceptions/GlobalHandlerException.java
@@ -0,0 +1,24 @@
+package com.meli.testcasesjpa.exceptions;
+
+import com.meli.testcasesjpa.model.response.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalHandlerException {
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleNotFoundException(NotFoundException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponse(e.getMessage()));
+    }
+
+    // * =========
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<ErrorResponse> handleBadRequestException(BadRequestException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse(e.getMessage()));
+    }
+
+}
---
+++ 9.  implementacion de bases de datos/TestCasesJpa/src/main/java/com/meli/testcasesjpa/model/response/ErrorResponse.java
@@ -0,0 +1,12 @@
+package com.meli.testcasesjpa.model.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ErrorResponse {
+    private String message;
+}
