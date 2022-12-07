27e250c3d849a705bca4d0f970220b37a9a487b4
ayrestrepo <ayde.restrepo@mercadolibre.com.co>
2022-12-06 20:33:28 -0500

    Ejercicio 1 - Configurando JPA

---
+++ 9.  implementacion de bases de datos/MiniSeries/src/main/java/example/MiniSeries/repository/IMiniSerieRepository.java
@@ -0,0 +1,9 @@
+package example.MiniSeries.repository;
+
+import example.MiniSeries.model.MiniSerie;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IMiniSerieRepository extends JpaRepository<MiniSerie,Long>{
+}
