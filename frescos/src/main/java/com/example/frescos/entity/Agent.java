0ef862cc792764abd0d7604f1978c3a605402784
mainfantino <marcos.infantino@mercadolibre.com>
2022-12-26 10:26:34 -0300

    constructores buyer y agent

--- frescos/src/main/java/com/example/frescos/entity/Agent.java
+++ frescos/src/main/java/com/example/frescos/entity/Agent.java
@@ -5,13 +5,21 @@ import javax.persistence.*;
 import com.example.frescos.enums.ApplicationRol;
 import lombok.Data;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+
+import static com.example.frescos.enums.ApplicationRol.AGENT;
 
 @Data
 @Entity
 public class Agent extends ApplicationUser {
-    public Agent(Long id, String fullName, List<ApplicationRol> roles) {
-        super(id, fullName, roles);
+    public Agent(Long id, String fullName) {
+        super(id, fullName, new ArrayList<>(Arrays.asList(new ApplicationRol[]{AGENT})));
+    }
+
+    public Agent(String fullName) {
+        super(fullName, new ArrayList<>(Arrays.asList(new ApplicationRol[]{AGENT})));
     }
 
     public Agent() {
--- frescos/src/main/java/com/example/frescos/entity/Buyer.java
+++ frescos/src/main/java/com/example/frescos/entity/Buyer.java
@@ -3,14 +3,23 @@ package com.example.frescos.entity;
 import com.example.frescos.enums.ApplicationRol;
 import lombok.Data;
 import javax.persistence.Entity;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+
+import static com.example.frescos.enums.ApplicationRol.AGENT;
+import static com.example.frescos.enums.ApplicationRol.BUYER;
 
 
 @Data
 @Entity
 public class Buyer extends ApplicationUser {
-    public Buyer(Long id, String fullName, List<ApplicationRol> roles) {
-        super(id, fullName, roles);
+    public Buyer(Long id, String fullName) {
+        super(id, fullName, new ArrayList<>(Arrays.asList(new ApplicationRol[]{BUYER})));
+    }
+
+    public Buyer( String fullName) {
+        super(fullName, new ArrayList<>(Arrays.asList(new ApplicationRol[]{BUYER})));
     }
 
     public Buyer() {
