0a8008a642a734d0fc959a14e8c08906f4d99335
mainfantino <marcos.infantino@mercadolibre.com>
2022-12-26 10:22:09 -0300

    Roles

--- frescos/src/main/java/com/example/frescos/entity/Buyer.java
+++ frescos/src/main/java/com/example/frescos/entity/Buyer.java
@@ -1,5 +1,6 @@
 package com.example.frescos.entity;
 
+import com.example.frescos.enums.ApplicationRol;
 import lombok.Data;
 import javax.persistence.Entity;
 import java.util.List;
@@ -8,7 +9,7 @@ import java.util.List;
 @Data
 @Entity
 public class Buyer extends ApplicationUser {
-    public Buyer(Long id, String fullName, List<ApplicationUser> roles) {
+    public Buyer(Long id, String fullName, List<ApplicationRol> roles) {
         super(id, fullName, roles);
     }
 
