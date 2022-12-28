24e0469438af7a35ed2994d7ef0f42806d7e00d5
LauraGutierrezB <laura.gutierrezbustos@mercadolibre.com.co>
2022-12-28 09:01:22 -0500

    Test troubleshooting

--- frescos/src/test/java/com/example/frescos/integration/ProductIntegrationTest.java
+++ frescos/src/test/java/com/example/frescos/integration/ProductIntegrationTest.java
@@ -5,6 +5,7 @@ import com.example.frescos.dtos.SectionDTO;
 import com.example.frescos.dtos.WarehouseDTO;
 import com.example.frescos.utils.LoginManager;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.Arrays;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,9 +21,6 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import javax.transaction.Transactional;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 @SpringBootTest
@@ -32,7 +30,6 @@ public class ProductIntegrationTest {
 
     @Autowired
     private MockMvc mockMvc;
-    private static ObjectMapper objectMapper = new ObjectMapper();
     private static String agentToken;
 
     private void loginAsAgent() throws Exception {
@@ -44,14 +41,36 @@ public class ProductIntegrationTest {
     public void getListProductOk() throws Exception {
         //Arrange
         loginAsAgent();
-        Integer sectionCode = 2;
-        Long warehoseCode = 2L;
-        Long productId = 10L;
-        List<BatchForStockDTO> batches = new ArrayList<>();
-
-        SectionDTO sectionDTO = new SectionDTO(sectionCode, warehoseCode);
-        WarehouseDTO warehouseExpect = new WarehouseDTO(sectionDTO, productId, batches);
-        String expect = objectMapper.writeValueAsString(warehouseExpect);
+        Long productId = 4L;
+        String expect = "{\n" +
+                "    \"section\": {\n" +
+                "        \"section_code\": 1,\n" +
+                "        \"warehouse_code\": 2\n" +
+                "    },\n" +
+                "    \"productoId\": 4,\n" +
+                "    \"batches\": [\n" +
+                "        {\n" +
+                "            \"batch_number\": 4,\n" +
+                "            \"current_quantity\": 4002,\n" +
+                "            \"due_date\": \"27-01-2023\"\n" +
+                "        },\n" +
+                "        {\n" +
+                "            \"batch_number\": 14,\n" +
+                "            \"current_quantity\": 8070,\n" +
+                "            \"due_date\": \"16-02-2023\"\n" +
+                "        },\n" +
+                "        {\n" +
+                "            \"batch_number\": 15,\n" +
+                "            \"current_quantity\": 10,\n" +
+                "            \"due_date\": \"26-02-2023\"\n" +
+                "        },\n" +
+                "        {\n" +
+                "            \"batch_number\": 13,\n" +
+                "            \"current_quantity\": 400,\n" +
+                "            \"due_date\": \"27-05-2023\"\n" +
+                "        }\n" +
+                "    ]\n" +
+                "}";
 
         // Matchers
         ResultMatcher expectedStatus = MockMvcResultMatchers.status().isOk();
@@ -63,7 +82,7 @@ public class ProductIntegrationTest {
         // Request
         MockHttpServletRequestBuilder requestPayload = MockMvcRequestBuilders
                 .get("/api/v1/fresh-products/{idProduct}/batch/list/", productId)
-                .param("order", "L")
+                .param("order", "F")
                 .contentType(MediaType.APPLICATION_JSON)
                 .header("Authorization", agentToken);
 
@@ -80,14 +99,37 @@ public class ProductIntegrationTest {
     public void getListProductOrderNull() throws Exception {
         ///Arrange
         loginAsAgent();
-        Integer sectionCode = 2;
-        Long warehoseCode = 2L;
-        Long productId = 10L;
-        List<BatchForStockDTO> batches = new ArrayList<>();
+        Long productId = 4L;
 
-        SectionDTO sectionDTO = new SectionDTO(sectionCode, warehoseCode);
-        WarehouseDTO warehouseExpect = new WarehouseDTO(sectionDTO, productId, batches);
-        String expect = objectMapper.writeValueAsString(warehouseExpect);
+        String expect = "{\n" +
+                "    \"section\": {\n" +
+                "        \"section_code\": 1,\n" +
+                "        \"warehouse_code\": 2\n" +
+                "    },\n" +
+                "    \"productoId\": 4,\n" +
+                "    \"batches\": [\n" +
+                "        {\n" +
+                "            \"batch_number\": 4,\n" +
+                "            \"current_quantity\": 4002,\n" +
+                "            \"due_date\": \"27-01-2023\"\n" +
+                "        },\n" +
+                "        {\n" +
+                "            \"batch_number\": 13,\n" +
+                "            \"current_quantity\": 400,\n" +
+                "            \"due_date\": \"27-05-2023\"\n" +
+                "        },\n" +
+                "        {\n" +
+                "            \"batch_number\": 14,\n" +
+                "            \"current_quantity\": 8070,\n" +
+                "            \"due_date\": \"16-02-2023\"\n" +
+                "        },\n" +
+                "        {\n" +
+                "            \"batch_number\": 15,\n" +
+                "            \"current_quantity\": 10,\n" +
+                "            \"due_date\": \"26-02-2023\"\n" +
+                "        }\n" +
+                "    ]\n" +
+                "}";
 
         // Matchers
         ResultMatcher expectedStatus = MockMvcResultMatchers.status().isOk();
@@ -113,6 +155,31 @@ public class ProductIntegrationTest {
     @Rollback
     @DisplayName("REQ3 - Consultar un producto con un order inválido")
     public void getListProductOrderBad() throws Exception {
+        //Arrange
+        loginAsAgent();
+        Long productId = 10L;
+
+        // Matchers
+        ResultMatcher expectedStatus = MockMvcResultMatchers.status().isBadRequest();
+
+        // Request
+        MockHttpServletRequestBuilder requestPayload = MockMvcRequestBuilders
+                .get("/api/v1/fresh-products/{idProduct}/batch/list/", productId)
+                .param("order", "Inválido")
+                .contentType(MediaType.APPLICATION_JSON)
+                .header("Authorization", agentToken);
+
+        //Act - Assert
+        mockMvc
+                .perform(requestPayload)
+                .andDo(print())
+                .andExpectAll(expectedStatus);
+    }
+
+    @Test
+    @Rollback
+    @DisplayName("REQ3 - Consultar un producto con una sección inválida")
+    public void getListProductNotFound() throws Exception {
         //Arrange
         loginAsAgent();
         Long productId = 10L;
